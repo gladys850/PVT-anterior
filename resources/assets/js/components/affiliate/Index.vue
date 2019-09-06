0b3e2b9398c800a191e92ef51fae2e38934e7139
magali <magalipomarivero@gmail.com>
2019-09-06 14:19:42 -0400

    affiliate list

---
+++ resources/assets/js/components/affiliate/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container fluid>
+    <v-toolbar dense color="tertiary">
+      <v-toolbar-title>Afiliados</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-flex xs2>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar"
+          single-line
+          hide-details
+          full-width
+          clearable
+        ></v-text-field>
+      </v-flex>
+    </v-toolbar>
+    <v-card>
+      <v-card-text>
+            <List :bus="bus"/>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+<script>
+
+//import _ from 'lodash'
+import Vue from 'vue'
+import List from '@/components/affiliate/List'
+
+export default {
+  name: "affiliateIndex",
+  components: {
+    List,
+  //  LdapAffiliates
+  },
+  data: () => ({
+    //viewType: 'Afiliados',
+    search: '',
+    bus: new Vue(),
+    //status: 'active'
+  }),
+  watch: {
+    search: _.debounce(function () {
+      this.bus.$emit('search', this.search)
+    }, 1000),
+  }
+}
+</script>
---
+++ resources/assets/js/components/affiliate/List.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="affiliates"
+    :loading="loading"
+    :options.sync="options"
+    :server-items-length="totalAffiliates"
+    :footer-props="{ itemsPerPageOptions: [10, 20, 40] }"
+    :fixed-header="true"
+    calculate-widths
+  >
+    <template v-slot:items="props">
+      <td class="text-xs-left">{{ props.item.first_name }}</td>
+      <td class="text-xs-left">{{ props.item.second_name}}</td>
+      <td class="text-xs-left">{{ props.item.last_name }}</td>
+      <td class="text-xs-left">{{ props.item.mothers_last_name }}</td>
+      <td class="text-xs-left">{{ props.item.identity_card }}</td>
+      <td>edita</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'affiliates-list',
+  props: ['bus'],
+  data: () => ({
+    loading: true,
+    search: '',
+    options: {
+      page: 1,
+      itemsPerPage: 10,
+      sortBy: null,
+      descending: false
+    },
+    affiliates: [],
+    totalAffiliates: 0,
+    headers: [
+     
+      { 
+        text: 'Nombre',
+        value: 'first_name', 
+        class: ['normal', 'white--text'],
+        width: '5%',
+        sortable: false 
+      },{ 
+        text: 'Apellido Paterno', 
+        value: 'last_name', 
+        class: ['normal', 'white--text'],
+        width: '5%',
+        sortable: false 
+      },{ text: 'Apellido Materno',
+        value: 'mothers_last_name', 
+        class: ['normal', 'white--text'],
+        width: '10%',
+        sortable: false 
+      },{ 
+        text: 'Nro. de CI',
+        value: 'identity_card',
+        class: ['normal', 'white--text'],
+        width: '15%',
+        sortable: false 
+      }
+    
+    ]
+
+  }),
+  watch: {
+    options: function(newVal, oldVal) {
+      if (newVal.page != oldVal.page || newVal.itemsPerPage != oldVal.itemsPerPage || newVal.sortBy != oldVal.sortBy || newVal.descending != oldVal.descending) {
+        this.getAffiliates()
+      }
+    },
+    search: function(newVal, oldVal) {
+      if (newVal != oldVal) {
+        this.getAffiliates()
+      }
+    },
+  },
+  mounted() {
+    this.bus.$on('search', val => {
+      this.search = val
+    })
+    this.getAffiliates()
+  },
+  methods: {
+    async getAffiliates(params) {
+      try {
+        this.loading = true
+        let res = await axios.get(`affiliate`, {
+          params: {
+            page: this.options.page,
+            per_page: this.options.itemsPerPage,
+            sortBy: this.options.sortBy,
+            direction: this.options.descending ? 'desc' : 'asc',
+            search: this.search
+          }
+        })
+        this.affiliates = res.data.data
+        this.totalAffiliates = res.data.total
+        delete res.data['data']
+        this.options.page = res.data.current_page
+        this.options.itemsPerPage = parseInt(res.data.per_page)
+        this.options.totalItems = res.data.total
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
+
