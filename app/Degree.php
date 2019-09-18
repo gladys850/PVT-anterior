cb9a29e7944498f67d3f4dd63ff132bb828d9be6
magali <magalipomarivero@gmail.com>
2019-09-18 15:23:41 -0400

    Models adicionados

---
+++ app/Degree.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Degree extends Model
+{
+    protected $fillable = [
+        'hierarchy_id',
+        'code',
+        'name',
+        'shortened'
+    ];
+
+    public function affiliates()
+    {
+        return $this->hasMany(Affiliate::class);
+    }
+    
+}
+
