dc5e611f91b24e4f2dc1b3fd0e1e68a683ad6c6f
magali <magalipomarivero@gmail.com>
2019-10-01 10:55:10 -0400

    adicion de funciones para verificar si un archivo *.png existe conexion ftp

--- app/Affiliate.php
+++ app/Affiliate.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Laratrust\Traits\LaratrustUserTrait;
+use Illuminate\Support\Facades\Storage;
+
 class Affiliate extends Model
 {
     use Traits\EloquentGetTableNameTrait;
@@ -47,52 +49,55 @@ class Affiliate extends Model
  
     public function getPictureSavedAttribute()
     {
-        return false;
+        return Storage::disk('ftp')->exists($this->id.'_perfil.png');
     }
-
     public function getFingerprintSavedAttribute()
     {
-        return true;
+        $fingerprint_pictures = ['_left_four.png', '_right_four.png', '_thumbs.png'];
+        $fingerprint_exists = false;
+        foreach ($fingerprint_pictures as $picture) {
+          $fingerprint_exists |= Storage::disk('ftp')->exists($this->id.$picture);
+        }
+        return $fingerprint_exists;
     }
-
     public function getFullNameAttribute()
     {
       return preg_replace('/[[:blank:]]+/', ' ', join(' ', [$this->first_name, $this->second_name, $this->last_name, $this->mothers_last_name]));
     }
 
-      public function category()
-      {
-        return $this->belongsTo(Category::class);
-      }
-      public function degree()
-      {
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
+    public function degree()
+    {
       return $this->belongsTo(Degree::class);
-      }
-      public function city_identity_card()
-      {
-        return $this->belongsTo(City::class,'city_identity_card_id', 'id');
-      }
-      public function affiliate_state()
-      {
-          return $this->belongsTo(AffiliateState::class);
-      }
-      public function city_birth()
-      {
-          return $this->belongsTo(City::class, 'city_birth_id', 'id');
-      }
-      public function pension_entity()
-      {
-          return $this->belongsTo(PensionEntity::class);
-      }
+    }
+    public function city_identity_card()
+    {
+      return $this->belongsTo(City::class,'city_identity_card_id', 'id');
+    }
+    public function affiliate_state()
+    {
+      return $this->belongsTo(AffiliateState::class);
+    }
+    public function city_birth()
+    {
+      return $this->belongsTo(City::class, 'city_birth_id', 'id');
+    }
+    public function pension_entity()
+    {
+      return $this->belongsTo(PensionEntity::class);
+    }
       // add records 
-      public function records()
-      {
-          return $this->morphMany(Record::class, 'recordable');
-      }
+    public function records()
+    {
+      return $this->morphMany(Record::class, 'recordable');
+    }
       //address
-      public function address()
-      {
-        return $this->morphToMany(Address::class, 'addressable');
-      }
+    public function address()
+    {
+      return $this->morphToMany(Address::class, 'addressable');
+    }
 
 }
