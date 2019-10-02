cc48d537438328d1572047e4256a02551f160479
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-10-02 15:13:42 -0400

    Added floating button to affiliate add component

--- resources/assets/js/components/affiliate/Add.vue
+++ resources/assets/js/components/affiliate/Add.vue
@@ -1,100 +1,141 @@
 <template>
-  <v-container fluid>
-  <div justify="space-around">
-    <v-tabs
-      v-model="tab"
-      background-color="deep-blue accent-2"
-      class="elevation-2"
-      dark
-      :vertical="vertical"
-      :icons-and-text="icons"
-    >
-      <v-tabs-slider></v-tabs-slider>
+  <v-card flat>
+    <v-card-title>
+      <v-toolbar dense color="tertiary" style="z-index: 1;">
+        <v-toolbar-title>
+          <Breadcrumbs/>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab
+              dark
+              small
+              color="success"
+              bottom
+              right
+              absolute
+              v-on="on"
+              style="margin-right: -9px;"
+              :to="{ name: 'affiliateAdd', params: { id:'new'} }"
+            >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+          <span>Activar edición</span>
+        </v-tooltip>
+      </v-toolbar>
+    </v-card-title>
+    <v-card-text>
+      <v-tabs
+        v-model="tab"
+        background-color="deep-blue accent-2"
+        class="elevation-2"
+        dark
+        :vertical="vertical"
+        :icons-and-text="icons"
+      >
+        <v-tabs-slider></v-tabs-slider>
 
-      <v-tab
-        :href="`#tab-1`"
-      >
-        <v-icon v-if="icons">mdi-trending-up</v-icon>
-      </v-tab>
-      <v-tab
-        :href="`#tab-2`"
-      >
-        <v-icon v-if="icons">mdi-account</v-icon>
-      </v-tab>
-       <v-tab
-        :href="`#tab-3`"
-      >
-        <v-icon v-if="icons">mdi-police-badge</v-icon>
-      </v-tab>
         <v-tab
-        :href="`#tab-4`"
-      >
-        <v-icon v-if="icons">mdi-account-heart</v-icon>
-      </v-tab>
+          :href="`#tab-1`"
+        >
+          <v-icon v-if="icons">mdi-trending-up</v-icon>
+        </v-tab>
+        <v-tab
+          :href="`#tab-2`"
+        >
+          <v-icon v-if="icons">mdi-account</v-icon>
+        </v-tab>
+        <v-tab
+          :href="`#tab-3`"
+        >
+          <v-icon v-if="icons">mdi-police-badge</v-icon>
+        </v-tab>
           <v-tab
-        :href="`#tab-5`"
-      >
-        <v-icon v-if="icons">mdi-fingerprint</v-icon>
-      </v-tab>
+          :href="`#tab-4`"
+        >
+          <v-icon v-if="icons">mdi-account-heart</v-icon>
+        </v-tab>
+            <v-tab
+          :href="`#tab-5`"
+        >
+          <v-icon v-if="icons">mdi-fingerprint</v-icon>
+        </v-tab>
 
-      <v-tab-item
-        :value="'tab-1'"
-      >
-        <v-card flat tile >
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item
-        :value="'tab-2'"
-      >
-        <v-card flat tile >
-          <v-card-text><Profile/></v-card-text>
-        </v-card>
-      </v-tab-item>
         <v-tab-item
-        :value="'tab-3'"
-      >
-        <v-card flat tile >
-          <v-card-text>{{ text2 }}</v-card-text>
-        </v-card>
-      </v-tab-item>
+          :value="'tab-1'"
+        >
+          <v-card flat tile >
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
         <v-tab-item
-        :value="'tab-4'"
-      >
-        <v-card flat tile >
-          <v-card-text>{{ text3 }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-        <v-tab-item
-        :value="'tab-5'"
-      >
-        <v-card flat tile >
-         <v-card-text> {{ text4 }} </v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
-  </div>
-  </v-container>
+          :value="'tab-2'"
+        >
+          <v-card flat tile >
+            <v-card-text><Profile/></v-card-text>
+          </v-card>
+        </v-tab-item>
+          <v-tab-item
+          :value="'tab-3'"
+        >
+          <v-card flat tile >
+            <v-card-text>{{ text2 }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+          <v-tab-item
+          :value="'tab-4'"
+        >
+          <v-card flat tile >
+            <v-card-text>{{ text3 }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+          <v-tab-item
+          :value="'tab-5'"
+        >
+          <v-card flat tile >
+          <v-card-text> {{ text4 }} </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card-text>
+  </v-card>
 </template>
 <script>
+import Breadcrumbs from '@/components/shared/Breadcrumbs'
 import Profile from './Profile'
-  export default {
-    name: "affiliate-index",
-    components: {
-        Profile
-    },
-    data () {
-      return {
-        tab: null,
-        text: 'hola',
-        text1: 'como ',
-        text2: 'estas',
-        text3: 'chau',
-        text4: 'huella',
-        icons: true,
-        vertical: true,
-        tabs: 10,
+
+export default {
+  name: "affiliate-index",
+  components: {
+    Breadcrumbs,
+    Profile
+  },
+  data () {
+    return {
+      tab: null,
+      text: 'hola',
+      text1: 'como ',
+      text2: 'estas',
+      text3: 'chau',
+      text4: 'huella',
+      icons: true,
+      vertical: true,
+      tabs: 3
+    }
+  },
+  beforeMount() {
+    this.$store.commit('setBreadcrumbs', [
+      {
+        text: 'Afiliados',
+        to: { name: 'affiliateIndex' }
+      }, {
+        text: 'Nuevo',
+        to: { name: 'affiliateAdd', params: { id:'new'} }
       }
-    },
+    ])
   }
+}
 </script>
