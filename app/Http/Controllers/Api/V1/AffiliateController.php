6a99f48e8fd6520bc77c70571aad5aff2e3be17f
lore <darlin.lore@gmail.com>
2019-10-09 18:08:20 -0400

    webcam route

--- app/Http/Controllers/Api/V1/AffiliateController.php
+++ app/Http/Controllers/Api/V1/AffiliateController.php
@@ -182,7 +182,7 @@ class AffiliateController extends Controller
     $image = str_replace('data:image/jpeg;base64,', '', $image);
     $image = str_replace(' ', '+', $image);
     $imageName = $code.'_perfil.'.'jpg';
-    Storage::disk('ftp')->put($base_path,$imageName,base64_decode($image));
+    Storage::disk('ftp')->put($imageName,base64_decode($image),$base_path);
 
 }
     
