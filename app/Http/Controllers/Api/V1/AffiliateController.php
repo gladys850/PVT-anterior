3ebd751473d59e47e2c1a14c689ba2d93e5bdb4b
lore <darlin.lore@gmail.com>
2019-10-10 08:28:41 -0400

    webcam save route

--- app/Http/Controllers/Api/V1/AffiliateController.php
+++ app/Http/Controllers/Api/V1/AffiliateController.php
@@ -184,7 +184,8 @@ class AffiliateController extends Controller
     $image = str_replace('data:image/jpeg;base64,', '', $image);
     $image = str_replace(' ', '+', $image);
     $imageName = $code.'_perfil.'.'jpg';
-    Storage::disk('ftp')->put($imageName,base64_decode($image),$base_path);
+
+    Storage::disk('ftp')->put($base_path.$imageName, base64_decode($image));
 
     }
     //get information spouse
