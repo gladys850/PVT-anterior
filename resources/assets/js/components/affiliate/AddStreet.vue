dc161f56e686034b790f36c2e57eb7fd3373ae4b
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-10-14 17:51:24 -0400

    Add bus to affiliate profile

--- resources/assets/js/components/affiliate/AddStreet.vue
+++ resources/assets/js/components/affiliate/AddStreet.vue
@@ -1,33 +1,46 @@
 <template>
-  <v-container fluid >
+  <v-dialog
+    v-model="dialog"
+    width="500"
+  >
+    <v-card>
+      <v-toolbar dense flat color="tertiary">
+        <v-toolbar-title>Añadir Dirección</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click.stop="close()">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-title></v-card-title>
+      <v-card-text>
+        <v-container fluid >
       <v-row justify="center">
         <v-col cols="12" >
-              <v-container class="py-0">
+          <v-container class="py-0">
                 <v-row>
-                    <v-col cols="12" md="5" >
-                      <v-select
-                        data-vv-name="Ciudad"
-                        :items="cities"
-                        item-text="name"
-                        item-value="id"
-                        :loading="loading"
-                        label="Ciudad"
-                        v-model="addresses.city_address_id"
-                      ></v-select>
+                  <v-col cols="12" md="5" >
+                    <v-select
+                      data-vv-name="Ciudad"
+                      :items="cities"
+                      item-text="name"
+                      item-value="id"
+                      label="Ciudad"
+                      v-model="address.city_address_id"
+                    ></v-select>
                     </v-col>
-                    <v-col cols="12" md="7">
-                      <v-text-field
-                      v-model="addresses.zone"
+                  <v-col cols="12" md="7">
+                    <v-text-field
+                      v-model="address.zone"
                       label="Zona"
                       class="purple-input"
                       v-validate.initial="'min:1|max:20'"
                       :error-messages="errors.collect('celular')"
                       data-vv-name="zona"
-                      ></v-text-field>
+                    ></v-text-field>
                     </v-col>
                     <v-col cols="12" md="9">
                       <v-text-field
-                      v-model="addresses.street"
+                      v-model="address.street"
                       label="Calle/Avenida"
                       class="purple-input"
                       v-validate.initial="'min:1|max:20'"
@@ -37,7 +50,7 @@
                     </v-col>
                     <v-col cols="12" md="3">
                       <v-text-field
-                      v-model="addresses.number_address"
+                      v-model="address.number_address"
                       label="Nro"
                       class="purple-input"
                       v-validate.initial="'min:1|max:20'"
@@ -50,51 +63,53 @@
         </v-col>
       </v-row>
   </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click.stop="close()"
+          color="error"
+          :disabled="errors.any()"
+        >Guardar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
   export default {
   name: "affiliate-addresses",
-  data: () => ({
-    loading: true,
-    addresses: {
-    zone: null,
-    street:null,
-    number_address: null,
-    city_address_id:null,
+  props: {
+    bus: {
+      type: Object,
+      required: true
     },
-    cities: [],
-      }),
-  beforeMount() {
-    this.getCities();
-  },
-  mounted() {
-    if (this.$route.params.id != 'new') {
-      this.getAffiliate(this.$route.params.id)
+    cities: {
+      type: Array,
+      required: true
     }
   },
-  methods: {
-    async getCities() {
-    try {
-      this.loading = true
-      let res = await axios.get(`city`);
-      this.cities = res.data;
-    } catch (e) {
-      this.dialog = false;
-      console.log(e);
-    }finally {
-        this.loading = false
-      }
+  data: () => ({
+    dialog: false,
+    address: {},
+  }),
+  mounted() {
+    this.bus.$on('openDialog', (address) => {
+      this.address = address
+      this.dialog = true
+    })
   },
-    async getAffiliate(id) {
-      try {
-        this.loading = true
-        let res = await axios.get(`affiliate/${id}`)
-        this.affiliate = res.data
-      } catch (e) {
-        console.log(e)
-      } finally {
-        this.loading = false
+  methods: {
+    close() {
+      this.saveAddress()
+      this.bus.$emit('saveAddress', this.address)
+      this.dialog = false
+      this.address = {}
+    },
+    saveAddress() {
+      if (this.address.id) {
+        console.log('PATCH editando')
+      } else {
+        console.log('POST creando')
       }
     }
   }
