8f0194ed39659f842659703c06ac36b168af194c
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-10-22 10:50:56 -0400

    Added geolocation to affiliate address form, closed #120

--- resources/assets/js/components/shared/Loading.vue
+++ resources/assets/js/components/shared/Loading.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container fluid fill-height class="my-5 py-5">
     <v-layout row align-center justify-center>
-      <v-progress-circular
+      <v-progress-linear
         :size="100"
         :width="8"
         color="primary"
         indeterminate
-      ></v-progress-circular>
+        rounded
+      ></v-progress-linear>
     </v-layout>
   </v-container>
 </template>
