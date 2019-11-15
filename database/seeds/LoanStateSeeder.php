6744c697aeb0a7b92cbf45eb8e568d7febca60ae
Maritza <marycqmia@gmail.com>
2019-11-15 13:18:53 -0400

    comit corregido de LoanStateSeeder

--- database/seeds/LoanStateSeeder.php
+++ database/seeds/LoanStateSeeder.php
@@ -14,18 +14,18 @@ class LoanStateSeeder extends Seeder
     public function run()
     {
         $states = [
-            ['name' => 'Mora','description' => 'Retraso de la cuota a la fecha establecido' ],
-            ['name' => 'En Proceso','description' => 'Prestamo en tramite' ],
-            ['name' => 'Aprobado ','description' => 'Solicitud de prestamo Aprobado' ],
-            ['name' => 'Anulado ','description' => 'Tramite del prestamo Anulado' ],
-            ['name' => 'Rechazado ','description' => 'Tramite del prestamo Rechazado' ],
-            ['name' => 'Desembolsado ','description' => 'Dinero Desembolsado' ],
-            ['name' => 'Liquidado ','description' => 'Pago de la deuda en su totalidad' ],
-            ['name' => 'Amortizando ','description' => 'Pagando Cuotas' ]
+            [['name' => 'Mora'],['description' => 'Retraso de la cuota a la fecha establecido']],
+            [['name' => 'En Proceso'],['description' => 'Prestamo en tramite' ]],
+            [['name' => 'Aprobado '],['description' => 'Solicitud de prestamo Aprobado' ]],
+            [['name' => 'Anulado '],['description' => 'Tramite del prestamo Anulado' ]],
+            [['name' => 'Rechazado '],['description' => 'Tramite del prestamo Rechazado' ]],
+            [['name' => 'Desembolsado '],['description' => 'Dinero Desembolsado' ]],
+            [['name' => 'Liquidado '],['description' => 'Pago de la deuda en su totalidad' ]],
+            [['name' => 'Amortizando '],['description' => 'Pagando Cuotas' ]]
             
         ];
         foreach ($states as $state) {
-            LoanState::Create($state);
+            LoanState::firstOrcreate($state[0],$state[1]);
         }
         
     }
--- version-seeder.sh
+++ version-seeder.sh
@@ -13,4 +13,5 @@ php artisan db:seed --class=UserPermissionSeeder
 php artisan db:seed --class=RecordPermissionSeeder
 php artisan db:seed --class=AffiliatePermissionSeeder
 php artisan db:seed --class=LoanReceiptRoleSeeder
+php artisan db:seed --class=LoanStateSeeder
 php artisan db:seed --class=AdminSeeder
