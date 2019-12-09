31dd2f5501e9304bd0f857586fd4ef0857d8e3f4
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-12-09 08:21:16 -0400

    Added loan/:hash route

---
+++ resources/assets/js/components/loan/Add.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ $route.params.hash }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loan-add",
+  computed: {
+    isNew() {
+      return this.$route.params.hash == 'new'
+    }
+  }
+}
+</script>
