bd00ad8a71fe33203b3914873ed2d1e1d8c1665b
magali <magalipomarivero@gmail.com>
2020-02-04 09:08:33 -0400

    Modificacion ModuleController

--- database/seeds/LoanIntervalSeeder.php
+++ database/seeds/LoanIntervalSeeder.php
@@ -20,9 +20,9 @@ class LoanIntervalSeeder extends Seeder
             $pro_type[$i] = $pro->id; $i++; 
         }
         $loan_intervals = [
-            ['maximum_amount' => 25000,'minimum_amount' => 2001,'maximum_term' => 24,'minimum_term' => 4,'procedure_type_id' => $pro_type[1]],
+            ['maximum_amount' => 25000,'minimum_amount' => 2001,'maximum_term' => 30,'minimum_term' => 3,'procedure_type_id' => $pro_type[1]],
             ['maximum_amount' => 150000,'minimum_amount' => 25001,'maximum_term' => 96,'minimum_term' => 25,'procedure_type_id' => $pro_type[2]],
-            ['maximum_amount' => 2000,'minimum_amount' => 200,'maximum_term' => 3,'minimum_term' => 1,'procedure_type_id' => $pro_type[3]],
+            ['maximum_amount' => 2000,'minimum_amount' => 200,'maximum_term' => 2,'minimum_term' => 1,'procedure_type_id' => $pro_type[3]],
             ['maximum_amount' => 700000,'minimum_amount' => 25001,'maximum_term' => 240,'minimum_term' => 25,'procedure_type_id' => $pro_type[4]],     
         ];
         foreach ($loan_intervals as $loan_interval) {
