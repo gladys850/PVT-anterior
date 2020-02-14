4bcce34204138cfba0f02649fc7e29dc01fb08bb
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2020-02-14 16:32:13 -0400

    Added address documentation

--- app/Http/Controllers/Api/V1/AddressController.php
+++ app/Http/Controllers/Api/V1/AddressController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\AddressForm;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/** @group Direcciones
+* Datos de las direcciones de los afiliados y de aquellas relacionadas con los trámites
+*/
 class AddressController extends Controller
 {
     /**
@@ -29,11 +32,28 @@ class AddressController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Nueva dirección
+    * Inserta nueva dirección
+    * @bodyParam city_address_id integer ID de ciudad del CI. Example: 4
+    * @bodyParam zone string Zona. Example: Chuquiaguillo
+    * @bodyParam street string Calle. Example: Av. Panamericana
+    * @bodyParam number_address integer Número de casa. Example: 45
+    * @bodyParam latitude float Latitud de acuerdo a OpenStreet Maps. Example: -16.495244595604056
+    * @bodyParam longitude float Longitud de acuerdo a OpenStreet Maps. Example: -68.13450627055796
+    * @authenticated
+    * @response
+    * {
+    *     "city_address_id": 4,
+    *     "zone": "Chuquiaguillo",
+    *     "street": "Av. Panamericana",
+    *     "number_address": 43,
+    *     "latitude": -16.495244595604056,
+    *     "longitude": -68.13450627055796,
+    *     "updated_at": "2020-02-14 14:38:00",
+    *     "created_at": "2020-02-14 14:38:00",
+    *     "id": 11805
+    * }
+    */
     public function store(AddressForm $request)
     {
         return Address::create($request->all());
@@ -62,12 +82,29 @@ class AddressController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Actualizar dirección
+    * Actualizar los datos de una dirección existente
+    * @urlParam address required ID de dirección. Example: 11805
+    * @bodyParam city_address_id integer ID de ciudad del CI. Example: 4
+    * @bodyParam zone string Zona. Example: Chuquiaguillo
+    * @bodyParam street string Calle. Example: Av. Panamericana
+    * @bodyParam number_address integer Número de casa. Example: 45
+    * @bodyParam latitude float Latitud de acuerdo a OpenStreet Maps. Example: -16.495244595604056
+    * @bodyParam longitude float Longitud de acuerdo a OpenStreet Maps. Example: -68.13450627055796
+    * @authenticated
+    * @response
+    * {
+    *     "city_address_id": 4,
+    *     "zone": "Chuquiaguillo",
+    *     "street": "Av. Panamericana",
+    *     "number_address": 43,
+    *     "latitude": -16.495244595604056,
+    *     "longitude": -68.13450627055796,
+    *     "updated_at": "2020-02-14 14:38:00",
+    *     "created_at": "2020-02-14 14:38:00",
+    *     "id": 11805
+    * }
+    */
     public function update(AddressForm $request, $id)
     {
         // echo('entro');
@@ -78,11 +115,23 @@ class AddressController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Eliminar dirección
+    * Eliminar una dirección solo en caso de que no este relacionada ningún trámite
+    * @urlParam address required ID de dirección. Example: 1077
+    * @authenticated
+    * @response
+    * {
+    *     "id": 1077,
+    *     "city_address_id": 4,
+    *     "zone": "Villa Tejada Triangular-El Alto",
+    *     "street": "11",
+    *     "number_address": "112",
+    *     "created_at": "2018-09-06 15:57:42",
+    *     "updated_at": "2018-09-06 17:44:45",
+    *     "updated_at":" 2020-02-14 14:38:00",
+    *     "created_at":" 2020-02-14 14:38:00"
+    * }
+    */
     public function destroy($id)
     {
         $address = Address::findOrFail($id);
