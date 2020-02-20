9b8c8703b3a77ff6bcc33d5ec5d549e99358dc96
magali <magalipomarivero@gmail.com>
2020-02-20 11:52:11 -0400

    Impresion de contrato

--- resources/views/loan/contracts/advance.blade.php
+++ resources/views/loan/contracts/advance.blade.php
@@ -5,37 +5,35 @@
     <title>PLATAFORMA VIRTUAL ADMINISTRATIVA - MUSERPOL </title>
     <link rel="stylesheet" href="{{ public_path("/css/report-print.min.css") }}" media="all"/>
 </head>
-
+@include('partials.header', $header)
 <body>
-    @include('partials.header', $header)
-
-    <div class="block">
-        <div class="font-semibold leading-tight text-center m-b-10 text-xs">
-            CONTRATO DE PRÉSTAMO XXXXXXX-PROCEDURE-MODALITY
-            <div>Nº {{ $title }}</div>
-        </div>
+<div class="block">
+    <div class="font-semibold leading-tight text-center m-b-10 text-xs">
+        CONTRATO DE PRÉSTAMO <font style="text-transform: uppercase;">{{ $title->name }}</font>
+        <div>Nº {{ $loan->code }}</div>
     </div>
-
+</div>
+<div class="block text-justify">
     <div>
-        Conste en el presente contrato de préstamo de XXXXXXX-PROCEDURE-MODALITY, que al solo reconocimiento de firmas y rúbricas ante autoridad competente será elevado a instrumento público, por lo que las partes que intervienen lo suscriben al tenor de las siguientes claúsulas y condiciones:
+        Conste en el presente contrato de préstamo de {{ $title->name }}, que al solo reconocimiento de firmas y rúbricas ante autoridad competente será elevado a Instrumento Público, por lo que las partes que intervienen lo suscriben al tenor de las siguientes claúsulas y condiciones:
     </div>
     <div>
-        PRIMERA.- (DE LAS PARTES): Intervienen en el presente contrato, por una parte como acreedor la Mutual de Servicios al Policía (MUSERPOL), representada legalmente por el Director General Ejecutivo Cnl. XXXX-NOMBRE-CORONEL con C.I. XXXXX-LP y su Director de Asuntos Administrativos Lic. XXXX-NOMBRE-LAZO con C.I. XXXXXX-LP, que para fines de este contrato en adelante se denominará MUSERPOL con domiciliio en la Z. Sopocachi, Av. 6 de Agosto Nº 2354 y por otra parte como deudor(a) XXXXX-GENERO(el la) XXXXXX-NOMBRE-DISBURSABLE, con C.I. XXXXXXX-LP, XXXXXX-ESTADO-CIVIL, mayor de edad, hábil por derecho, natural de XXXXXXX-CITY-BIRTH, vecino de XXXXXX-CITY-IDENTITY-CARD y con domicilio especial en XXXXXX-ADDRESS, en adelante XXXXX-GENDER-denomido/a PRESTATARIO.
+       <b>PRIMERA.- (DE LAS PARTES):</b> Intervienen en el presente contrato, por una parte como acreedor la Mutual de Servicios al Policía (MUSERPOL), representada legalmente por el Director General Ejecutivo Cnl. XXXX-NOMBRE-CORONEL con C.I. XXXXX-LP y su Director de Asuntos Administrativos Lic. XXXX-NOMBRE-LAZO con C.I. XXXXXX-LP, que para fines de este contrato en adelante se denominará MUSERPOL con domiciliio en la Z. Sopocachi, Av. 6 de Agosto Nº 2354 y por otra parte como {{$gender ? ($gender == 'M' ? ' DEUDOR ' : ' DEUDORA ') : ' DEUDOR(A) '}} {{ $lender }}, con C.I. {{ $identity_card }}, {{ $civil_status }}, mayor de edad, hábil por derecho, natural de {{ $city_birth }}, vecino de {{ $city_identity_card }} y con domicilio especial en {{ $address }}, en adelante {{$gender ? ($gender == 'M' ? ' denominado ' : ' denominada ') : ' denominado(a) '}} PRESTATARIO.
     </div>
     <div>
-        SEGUNDA.- (DEL OBJETO): El objeto del presente contrato es el préstamo de dinero que MUSERPOL otogra al PRESTATARIO conforme a niveles de aprobación respectivos, en la suma de Bs. XXXXXXX-AMOUNT-REQUESTED (LITERAL XXXXX-AMOUNT-REQUESTES).
+       <b>SEGUNDA.- (DEL OBJETO):</b>  El objeto del presente contrato es el préstamo de dinero que MUSERPOL otorga al PRESTATARIO conforme a niveles de aprobación respectivos, en la suma de Bs. {{ $loan->amount_request }} ({{ Util::convertir($loan->amount_request) }} Bolivianos).
     </div>
     <div>
-        TERCERA.- (DEL INTERÉS): El préstamo objeto del presente contrato, devengará un interés ordinario del XXXXX-CURRENT-INTEREST% anual sobre saldo deudos, el mismo que se recargará con el interés penal en caso de mora de una o más amortizaciones. Esta tasa de interés podrá ser modificada en cualquier momento de acuerdo a las condiciones financieras que adopte MUSERPOL.
+        <b>TERCERA.- (DEL INTERÉS):</b> El préstamo objeto del presente contrato, devengará un interés ordinario del {{ $loan->interest->annual_interest }} % anual sobre saldo deudor, el mismo que se recargará con el interés penal en caso de mora de una o más amortizaciones. Esta tasa de interés podrá ser modificada en cualquier momento de acuerdo a las condiciones financieras que adopte MUSERPOL.
     </div>
     <div>
-        CUARTA.- (DEL DESEMBOLSO, DEL PLAZO Y LA CUOTA DE AMORTIZACIÓN): El desembolso del préstamo se acredita mediante comprobante escrito en el que conste el abono efectuado a favor del PRESTATARIO, o a través de una cuenta bancaria del Banco Unión a solicitud del PRESTATARIO, reconociendo ambas partes que el amparo de este procedimiento se cumple satisfactoriamente la exigencia contenida en el artículo 1331 del Código de Comercio. El plazo fijo e inprorrogable para el cumplimiento de la obligación contraida por el PRESTATARIO en virtud al préstamo otorgado es de XXXXX-LOAN-TERM meses computables a partir de la fecha de desembolso. La cuota de amortización mensual es de Bs. XXXXX-CUOTA-CALCULADORA (XXXX-LITERAL).
+        <b>CUARTA.- (DEL DESEMBOLSO, DEL PLAZO Y LA CUOTA DE AMORTIZACIÓN):</b> El desembolso del préstamo se acredita mediante comprobante escrito en el que conste el abono efectuado a favor del PRESTATARIO, o a través de una cuenta bancaria del Banco Unión a solicitud del PRESTATARIO, reconociendo ambas partes que el amparo de este procedimiento se cumple satisfactoriamente la exigencia contenida en el artículo 1331 del Código de Comercio. El plazo fijo e inprorrogable para el cumplimiento de la obligación contraida por el PRESTATARIO en virtud al préstamo otorgado es de {{ $loan->loan_term }} meses computables a partir de la fecha de desembolso. La cuota de amortización mensual es de Bs. {{ $estimated_quota }} ({{ Util::convertir($estimated_quota) }} Bolivianos).
     </div>
     <div>
         Los intereses generados entre la fecha del desembolso del préstamo y la fecha del primer pago serán cobrados con la primera cuota; conforme los establece el reglamento de préstamos.
     </div>
     <div>
-        QUINTA.- (DE LA FORMA DE PAGO Y OTRAS CONTINGENGIAS): Para el cumplimiento estricto de la obligación (capital e intereses) el PRESTATARIO, autoriza expresamente a MUSERPOL practicar los descuentos respectivos de los haberes que percibe en forma mensual a través del Comando General de la Policía Boliviana.
+        <b>QUINTA.- (DE LA FORMA DE PAGO Y OTRAS CONTINGENGIAS):</b> Para el cumplimiento estricto de la obligación (capital e intereses) el PRESTATARIO, autoriza expresamente a MUSERPOL practicar los descuentos respectivos de los haberes que percibe en forma mensual a través del Comando General de la Policía Boliviana.
     </div>
     <div>
         Si por cualquier motivo la MUSERPOL estuviera imposibilitada de realizar el descuento por el medio señalado, el PRESTATARIO se obliga a cumplir con la cuota de amortización mediante pago directo en la Oficina Central de MUSERPOL de la ciudad de La Paz o efectuar depósito bancario en la cuenta fiscal de la MUSERPOL y enviar la boleta de depósito original a la Oficina Central inmediatamente; sin necesidad de previo aviso; caso contrario el PRESTATARIO se hará pasible al recargo correspondiente a los intereses que se generen al día de pago por la deuda contraida.
@@ -44,7 +42,7 @@
         Asi mismo el prestatario se compromete a hacer conocer oportunamente a MUSERPOL sobre la omisión del descuento mensual que se hubiera dado a efectos de solicitar al Comando General de la Policía Boliviana se regularice este descuento, sin perjuicio que realice el depósito directo del mes omitido, de acuerdo a lo estipulado en el párrafo precedente.
     </div>
     <div>
-        SEXTA.- (DERECHOS DEL PRESTATARIO): Conforme al Artículo 29 del Reglamento de Préstamos las partes reconocen expresamente como derechos irrenunciables del PRESTATARIO, lo siguiente:
+        <b>SEXTA.- (DERECHOS DEL PRESTATARIO):</b> Conforme al Artículo 29 del Reglamento de Préstamos las partes reconocen expresamente como derechos irrenunciables del PRESTATARIO, lo siguiente:
     </div>
     <div>
         <ol type="a">
@@ -56,7 +54,7 @@
         </ol>
     </div>
     <div>
-        SÉPTIMA.- (OBLIGACIONES DEL PRESTATARIO): Conforme al Artículo 30 del Reglamento de Préstamos, las partes reconocen expresamente como obligaciones del PRESTATARIO, lo siguiente:
+       <b>SÉPTIMA.- (OBLIGACIONES DEL PRESTATARIO):</b> Conforme al Artículo 30 del Reglamento de Préstamos, las partes reconocen expresamente como obligaciones del PRESTATARIO, lo siguiente:
     </div>
     <div>
         <ol type="a">
@@ -72,10 +70,10 @@
         </ol>
     </div>
     <div>
-        OCTAVA.- (DE LA GARANTÍA): El PRESTATARIO, garantiza el pago de lo adeudado con todos sus bienes, derechos y acciones habidos y por haber, presentes y futuros conforme determina el Art. 1335 del Código Civil y además con los beneficios del Fondo de Retiro Policial Solidario y Complemento Económico así como establece el Artículo 65 del Reglamento de Préstamos de la MUSERPOL.
+        <b>OCTAVA.- (DE LA GARANTÍA):</b> El PRESTATARIO, garantiza el pago de lo adeudado con todos sus bienes, derechos y acciones habidos y por haber, presentes y futuros conforme determina el Art. 1335 del Código Civil y además con los beneficios del Fondo de Retiro Policial Solidario y Complemento Económico así como establece el Artículo 65 del Reglamento de Préstamos de la MUSERPOL.
     </div>
     <div>
-        NOVENA.- (MODIFICACIÓN DE LA SITUACIÓN DEL PRESTATARIO): El PRESTATARIO, en caso de fallecimiento, retiro voluntario o retiro forzoso garantizan con la totalidad de los beneficios de Fondo de Retiro Policial Solidario y Complemento Económico otorgados por la MUSERPOL, el cumplimiento efectivo de la presente obligación; por cuanto la liquidación de dichos beneficios pasarán a cubrir el monto total de la obligación que resulte adecuada, más los intereses devengados en fecha, previas las formalidades de ley.
+        <b>NOVENA.- (MODIFICACIÓN DE LA SITUACIÓN DEL PRESTATARIO):</b> El PRESTATARIO, en caso de fallecimiento, retiro voluntario o retiro forzoso garantizan con la totalidad de los beneficios de Fondo de Retiro Policial Solidario y Complemento Económico otorgados por la MUSERPOL, el cumplimiento efectivo de la presente obligación; por cuanto la liquidación de dichos beneficios pasarán a cubrir el monto total de la obligación que resulte adecuada, más los intereses devengados en fecha, previas las formalidades de ley.
     </div>
     <div>
         En caso de que se haya modificado la situación del PRESTATARIO del servicio activo al sector pasivo de la Policía Boliviana teniendo un saldo deudor respecto del préstamo obtenido, este acepta amortizar la deuda con su Complemento Económico, en caso de corresponderle debiendo al efecto de solicitar la reprogramación conforme establece el Artículo 90 del Reglamento de Préstamos de la MUSERPOL, por lo que el saldo se sujetará en función al nuevo líquido pagable, estableciendose una correlativa modificación de los plazos y cuota según sea el caso.
@@ -84,28 +82,40 @@
         En caso de incumplimiento de los pagos mensuales estipulados en el presente contrato que generen mora de la obligación, el PRESTATARIO no tendrá dercho a acceder a otro crédito hasta la cancelación total de la deuda.
     </div>
     <div>
-        DÉCIMA.- (DE LA CONFORMIDAD Y ACEPTACIÓN): Por una parte en calidad de acreedora la MUSERPOL, representada por su Director General Ejecutivo Cnl. XXXXX-MAE y su Director de Asuntos Administrativos Lic. XXXX-NOMBRE-LAZO y por otra parte en calidad de deudor(a) XXXXX-GENERO(el la) XXXXXX-NOMBRE-DISBURSABLE de generales ya señaladas como PRESTATARIO; damos nuestra plena conformidad con todas y cada una de las cláusulas precedentes, obligándolos a su fiel y estricto cumplimiento. En señal de lo cual suscribimos el presente contrato de préstamo de dinero en manifestación de nuestra libre y espontánea voluntad y sin que medie vicio de consentimiento alguno.
+        <b>DÉCIMA.- (DE LA CONFORMIDAD Y ACEPTACIÓN):</b> Por una parte en calidad de acreedora la MUSERPOL, representada por su Director General Ejecutivo Cnl. XXXXX-MAE y su Director de Asuntos Administrativos Lic. XXXX-NOMBRE-LAZO y por otra parte en calidad de deudor(a) XXXXX-GENERO(el la) XXXXXX-NOMBRE-DISBURSABLE de generales ya señaladas como PRESTATARIO; damos nuestra plena conformidad con todas y cada una de las cláusulas precedentes, obligándolos a su fiel y estricto cumplimiento. En señal de lo cual suscribimos el presente contrato de préstamo de dinero en manifestación de nuestra libre y espontánea voluntad y sin que medie vicio de consentimiento alguno.
+    </div><br><br>
+    <div class="text-center">
+        <p class="center">
+        La Paz, {{ Carbon::parse($loan->request_date)->isoFormat('LL') }}
+        </p>
     </div>
+</div><br><br><br><br> 
+<div class="block">
     <div>
-        XXXXXXX-CITY_ID, XXXXXX_FECHA
+        <div class='text-center'>
+            _____________________________<br> 
+            {{ $lender }}<br>
+            C.I. {{ $identity_card }}<br>
+            PRESTATARIO<br><br>
+        </div>
     </div>
-    <div>
-        ______________________
-        XXXXXXX_NOMBRE-DISBURSABLE
-        XXXXXX_CI_DISBURSABLE
-        PRESTATARIO
+    <div>    
+        <div style='float:left' class='text-center px-75' >
+            _____________________________<br>
+            XXXXXXX_NOMBRE-LAZO <br>
+            C.I. {{ $identity_card }}<br>           
+            <b>Director de Asuntos Administrativos</b><br>
+             MUSERPOL
+        </div>
+        <div style='float:right' class='text-center px-75'>
+            _____________________________<br>
+            Cnl. XXXXX-MAE <br>
+            C.I. {{ $identity_card }}<br>           
+            <b>Director General Ejecutivo<b><br>
+            MUSERPOL
+        </div>
+         
     </div>
-    <div>
-        ______________________
-        XXXXXXX_NOMBRE-LAZO
-        Director de Asuntos Administrativos
-        MUSERPOL
-    </div>
-    <div>
-        ______________________
-        Cnl. XXXXX-MAE
-        Director General Ejecutivo
-        MUSERPOL
-    </div>
+</div>
 </body>
 </html>
