ddab71d9788269f91cdd9295d079b8341ab1d228
magali <magalipomarivero@gmail.com>
2020-02-27 11:34:45 -0400

    Modificacion Controlador loan

--- app/Http/Controllers/Api/V1/LoanController.php
+++ app/Http/Controllers/Api/V1/LoanController.php
@@ -559,6 +559,7 @@ class LoanController extends Controller
     * @authenticated
     * @response
     */
+    
     public function print_contract($id)
     {
         $loan = Loan::findOrFail($id);
