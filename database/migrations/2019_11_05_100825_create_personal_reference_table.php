c1fdf8f826f1106a499b782c55f66096c228a90d
magali <magalipomarivero@gmail.com>
2020-03-13 11:40:50 -0400

    closed #380, closed #379

--- database/migrations/2019_11_05_100825_create_personal_reference_table.php
+++ database/migrations/2019_11_05_100825_create_personal_reference_table.php
@@ -15,19 +15,16 @@ class CreatePersonalReferenceTable extends Migration
     {
         Schema::create('personal_references', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('city_identity_card_id')->nullable(); 
+            $table->unsignedBigInteger('city_identity_card_id')->nullable();
             $table->foreign('city_identity_card_id')->references('id')->on('cities');
             $table->string('identity_card');
             $table->string('last_name');
             $table->string('mothers_last_name')->nullable();
             $table->string('first_name');
-            $table->string('second_name')->nullable(); 
-            $table->string('surname_husband')->nullable(); 
-            $table->date('birth_date');
-            $table->enum('gender', ['M', 'F'])->nullable();
-            $table->enum('civil_status', ['C', 'S', 'V', 'D'])->nullable();
-            $table->string('phone_number')->nullable(); 
-            $table->string('cell_phone_number')->nullable(); 
+            $table->string('second_name')->nullable();
+            $table->string('surname_husband')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('cell_phone_number')->nullable();
             $table->timestamps();
         });
     }
