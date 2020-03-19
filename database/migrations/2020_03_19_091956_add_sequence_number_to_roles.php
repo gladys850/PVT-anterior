64255b88ee9ab47d33d20d0b1822fe9d71f97a28
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2020-03-19 10:22:45 -0400

    Added rollback function

--- database/migrations/2020_03_19_091956_add_sequence_number_to_roles.php
+++ database/migrations/2020_03_19_091956_add_sequence_number_to_roles.php
@@ -14,7 +14,7 @@ class AddSequenceNumberToRoles extends Migration
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
-        $table->Integer('sequence_number')->nullable();
+            $table->unsignedTinyInteger('sequence_number')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddSequenceNumberToRoles extends Migration
     public function down()
     {
         Schema::table('roles', function (Blueprint $table) {
-            //
+            $table->dropColumn(['sequence_number']);
         });
     }
 }
