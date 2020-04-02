ffe342541190fdef8465a7d97851683c790923d3
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2020-04-02 11:06:26 -0400

    Conflicts resolved

--- database/seeds/RoleNameSeeder.php
+++ database/seeds/RoleNameSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Role;
-use App\Module;
+
 class RoleNameSeeder extends Seeder
 {
     /**
@@ -13,22 +13,6 @@ class RoleNameSeeder extends Seeder
      */
     public function run()
     {
-        //adicion de roles
-        $module_id = Module::whereName('prestamos')->first()->id;
-        $new_roles = [
-            ['module_id' => $module_id, 'display_name' => 'Calificación', 'action' => 'Calificado'],
-            ['module_id' => $module_id, 'display_name' => 'Revisión Legal', 'action' => 'Revisado e Impresión'],
-            ['module_id' => $module_id, 'display_name' => 'Jefatura', 'action' => 'Revisado'],
-            ['module_id' => $module_id, 'display_name' => 'Revisión Dirección', 'action' => 'Revisado'],
-            ['module_id' => $module_id, 'display_name' => 'Aprobación Legal', 'action' => 'Aprobado y firma'],
-            ['module_id' => $module_id, 'display_name' => 'Aprobación Dirección', 'action' => 'Aprobado'],
-            ['module_id' => $module_id, 'display_name' => 'Tesorería', 'action' => 'Recepcionado'],
-            ['module_id' => $module_id, 'display_name' => 'Cobranzas', 'action' => 'Recepcionado'],
-        ];
-        foreach ($new_roles  as $new_role) {
-            Role::firstOrCreate($new_role);
-        }
-        //
         $roles = Role::get();
         foreach ($roles as $role)
         {
