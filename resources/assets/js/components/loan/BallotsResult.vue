eaf1b0b6f856074b11fb9a37332abd8d51166010
stephanieLQ <stephanielunaquevedo@gmail.com>
2020-04-21 15:08:56 -0400

    conflictos

--- resources/assets/js/components/loan/BallotsResult.vue
+++ resources/assets/js/components/loan/BallotsResult.vue
@@ -7,7 +7,7 @@
             <v-col cols="12">
               <v-container class="py-0">
                 <v-row>
-                  <v-col cols="12" class="py-0">Resultado para el Prestamo</v-col>
+                  <v-col cols="12" class="py-0">Resultado para el Prestamo </v-col>
                   <br />
                   <v-col cols="12" md="3">
                     <v-layout row wrap>
