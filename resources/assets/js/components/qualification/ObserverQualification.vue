813ff0dafc76cc749a8aecc2367cdcf3e51ec9bd
stephanieLQ <stephanielunaquevedo@gmail.com>
2020-05-11 11:55:39 -0400

    closed #189, closed #190, closed #531, closed #550, closed #551

--- resources/assets/js/components/qualification/ObserverQualification.vue
+++ resources/assets/js/components/qualification/ObserverQualification.vue
@@ -4,153 +4,130 @@
       <v-col cols="12" class="mb-0" >
         <v-row justify="center" class="mb-0">
           <v-col cols="12" class="mb-0">
-            <v-toolbar-title>HISTORIAL DEL TRAMITE</v-toolbar-title>
-              <v-data-table
-                :headers="headers"
-                :items="record"
-                :items-per-page="4"
-                
-                class="elevation-1"
-              >
-                <template v-slot:item="items">
-                  <tr>
-                    <td>{{items.item.user_id}}</td>
-                    <td>{{items.item.record_type_id}}</td>
-                    <td>{{items.item.recordable_id}}</td>
-                    <td>{{items.item.action}}</td>
-                  </tr>
-                </template>
-              </v-data-table>
-            </v-col>
-            <v-col cols="6">
-              <v-toolbar-title>OBSERVACIONES</v-toolbar-title>
-            </v-col>
-            <v-col cols="6" class="ma-0 pr-10">
-              <v-tooltip top>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    fab
-                    dark
-                    x-small
-                    :color="'success'"
-                    bottom
-                    right
-                    v-on="on"
-                    style="margin-right: 0px; margin-left: 0px; margin-top:10px; "
-                    @click.stop="dialog = true"
-                  >
-                    <v-icon>mdi-plus</v-icon>
-                  </v-btn>
-                </template>
-                <div>
-                  <span>Agregar Observacion</span>
-                </div>
-              </v-tooltip>
-            </v-col>
-            <v-dialog
-              v-model="dialog"
-              width="600"
+            <v-tabs
+              background-color="primary"
+              center-active
+              dark
             >
-              <v-card>
-                <v-card-title    
-                  class="headline primary text-white"
-                  >Adicionar Observación
-                </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="4">
-                      <label>Tipo de Observacion:</label>
-                    </v-col>
-                    <v-col cols="8">
-                      <v-select
-                        dense
-                        :items="observation_type"
-                        item-text="name"
-                        item-value="id"
-                        persistent-hint
-                      ></v-select>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        dense
-                        label="Descripcion"
-                      ></v-text-field>
-                    </v-col>                   
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="green darken-1"
-                  @click="saveObservation()"
-                >
-                  Guardar
-                </v-btn>
-                <v-btn
-                  color="red"
-                  @click="dialog = false"
-                >
-                  Cancelar
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-col cols="12">
-            <v-data-table
-              :headers="headers1"
-              :items="observation"
-              hide-default-footer
-              class="elevation-1"
-            >  
-              <template v-slot:item="items">
-                <tr>
-                  <td>{{items.item.user_id}}</td>
-                  <td>{{items.item.observation_type_id}}</td>
-                  <td>{{items.item.message}}</td>
-                  <td>{{items.item.date}}</td>
-                </tr>
-              </template>
-            </v-data-table>
+              <v-tab>Observaciones</v-tab>
+                <v-tab-item>
+                  <v-card flat tile >
+                    <v-card-text class="mb-0">
+                      <v-tooltip top>
+                        <template v-slot:activator="{ on }">
+                          <v-btn
+                            fab
+                            dark
+                            x-small
+                            :color="'success'"
+                            bottom
+                            right
+                            v-on="on"
+                            style="margin-right: 0px; margin-left: 950px; margin-top:-11px; "
+                            @click.stop="bus.$emit('openDialog', { edit:false })"
+                          >
+                            <v-icon>mdi-plus</v-icon>
+                          </v-btn>
+                        </template>
+                        <div>
+                          <span>Agregar Observacion</span>
+                        </div>
+                      </v-tooltip>
+                    </v-card-text>
+                  </v-card>
+                  <v-card flat tile >
+                    <v-card-text class="mb-0">
+                      <v-col cols="12" class="mb-0">
+                        <v-data-table
+                          :headers="headers1"
+                          :items="observations"
+                          :items-per-page="4"
+                          class="elevation-1"
+                        >
+                          <template v-slot:item="items">
+                            <tr>
+                              <td>{{items.item.user_id}}</td>
+                              <td>{{items.item.observation_type_id}}</td>
+                              <td>{{items.item.message}}</td>
+                              <td>{{items.item.date}}</td>
+                              <td>
+                                <v-tooltip top>
+                                  <template v-slot:activator="{ on }">
+                                    <v-btn
+                                      icon
+                                      small
+                                      color="info"
+                                      @click.stop="bus.$emit('openDialog', {...items.item, ...{edit: true}})"
+                                      v-on="on"
+                                    >    
+                                      <v-icon>mdi-pencil</v-icon>
+                                    </v-btn>
+                                  </template>
+                                  <span class="caption">Editar</span>
+                                </v-tooltip>   
+                              </td>
+                            </tr>
+                          </template>
+                        </v-data-table>
+                      </v-col>
+                    </v-card-text>
+                  </v-card>
+                </v-tab-item>
+              <v-tab>HISTORIAL DEL TRAMITE </v-tab>
+                <v-tab-item>
+                  <v-card flat tile >
+                    <v-card-text>
+                      <v-col cols="12" class="mb-0">
+                        <v-data-table
+                          :headers="headers"
+                          :items="record"
+                          :items-per-page="4"
+                          class="elevation-1"
+                        >
+                          <template v-slot:item="items">
+                            <tr>
+                              <td>{{items.item.user_id}}</td>
+                              <td>{{items.item.record_type_id}}</td>
+                              <td>{{items.item.recordable_id}}</td>
+                              <td>{{items.item.action}}</td>
+                            </tr>
+                          </template>
+                        </v-data-table>
+                      </v-col>
+                    </v-card-text>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs>
           </v-col>
-          <v-col cols="3">
-            <v-toolbar-title class="text-left">ACCION DE LA TAREA :</v-toolbar-title>
-          </v-col>
-          <v-col cols="2" >
-            <v-select
-              dense
-              v-model="loanTypeSelected"
-              :onchange="Onchange()"
-              :items="lista"
-              item-text="name"
-              item-value="id"
-              persistent-hint
-            ></v-select>
-          </v-col> 
-          <v-col v-show="visible" cols="4" >
-            <v-select
-              dense
-              :items="loanFlow.previous"
-              item-text="id"
-              item-value="id"
-              persistent-hint
-            ></v-select>
-          </v-col>
-          <v-col v-show="visible1" cols="4" >
-            <v-select
-              dense
-              :items="loanFlow.next"
-              item-text="id"
-              item-value="id"
-              persistent-hint
-            ></v-select>
-          </v-col>  
-         <v-col v-show="visible2" cols="4" class="py-0">
+
+<v-col cols="4">
+        <v-select
+          label="Devolver"
+  v-model="valDevolver"
+       
+        
+          
+        
+          item-text="text"
+          item-value="value"
+        ></v-select>
+      </v-col>
+      <v-col cols="1">
+        <v-btn small color="danger" @click.stop>Devolver</v-btn>
+      </v-col>
+
+
+           <v-col cols="1" class="py-0">
             <v-checkbox    
-              :label="`Revisado`"
+              :label="`Revisar`"
+              v-model="valor"
+@click.stop="save()"
             ></v-checkbox>
-          </v-col>   
+          </v-col> 
+           <v-col cols="1">
+        <v-btn small color="success" @click.stop>Guardar</v-btn>
+      </v-col>
+             
           <v-spacer></v-spacer>
           <v-col cols="10"></v-col>
           <v-col cols="2" class="py-0">
@@ -159,22 +136,43 @@
               @click.stop="saveLoanRol()">Finalizar</v-btn>   
             </v-col>
           </v-row>
+
         </v-col>
       </v-row>
+                 <AddObservation :bus="bus"/>
   </v-container>
+    
+   
 </template>
 
 <script>
+
+import AddObservation from '@/components/qualification/AddObservation'
+
 export default {
   name: "observer-qualification",
 
+   components: {
+    AddObservation
+  },
+
   data: () => ({
+     valDevolver: null,
     loanFlow:[],
+    valor:true,
+   
     observation_type:[],
     loanTypeSelected:null,
+    observationSelected:null,
+    observation_description:null,
     visible:false,
     visible1:false,
     visible2:false,
+    listAreas: [],
+    valDerivar: null,
+    flow: {},
+    mapAreas: null,
+    bus: new Vue(),
     lista:[
     {
       id:'1',
@@ -192,28 +190,58 @@ export default {
     
     dialog: false,
     headers: [
-        { text: 'Usuario', align: 'left', value: 'user_id' },
-        { text: 'Record', align: 'left', value: 'record_type_id' },
-        { text: 'Nombre', align: 'left', value: 'recordable_id' },
-        { text: 'Accion', align: 'left', value: 'accion' }
+        { text: 'Usuario',class: ['normal', 'white--text'], align: 'left', value: 'user_id' },
+        { text: 'Record',class: ['normal', 'white--text'], align: 'left', value: 'record_type_id' },
+        { text: 'Nombre',class: ['normal', 'white--text'], align: 'left', value: 'recordable_id' },
+        { text: 'Accion',class: ['normal', 'white--text'], align: 'left', value: 'accion' }
         ],
     headers1: [
-        { text: 'Usuario', align: 'left', value: 'user_id' },
-        { text: 'Observacion', align: 'left', value: 'observation_type_id' },
-        { text: 'Mensaje', align: 'left', value: 'message' },
-        { text: 'Fecha', align: 'left', value: 'date' }
+        { text: 'Usuario', class: ['normal', 'white--text'], align: 'left', value: 'user_id' },
+        { text: 'Observacion', class: ['normal', 'white--text'], align: 'left', value: 'observation_type_id' },
+        { text: 'Mensaje', class: ['normal', 'white--text'], align: 'left', value: 'message' },
+        { text: 'Fecha', class: ['normal', 'white--text'], align: 'left', value: 'date' },
+        { text: 'Acciones', class: ['normal', 'white--text'], align: 'center', width: '11%', sortable: false
+      }
         ],
     
   }),
+  props: {
+    observations: {
+      type: Array,
+      required: true
+    },
+     record: {
+      type: Array,
+      required: true
+    }},
   
   beforeMount() {
-    this.getFlow(1)
-    this.getRecords()
+   
+  
     this.getObservationType()
-    this.getObservation(1)
+   
+  },
+  mounted(){
+      this.getFlow()
+       this.bus.$on('saveObservation', (observation) => {
+        console.log("entro al bus"+observation)
+      
+        
+            this.observations.unshift(observation)
+       
+        
+
+
+
+    })
+  
   },
   
   methods: {
+    save(){
+      this.valor=false,
+      console.log("se grabo")
+    },
     Onchange(){
       for (this.i = 0; this.i< this.lista.length; this.i++) {
         if(this.loanTypeSelected==1)
@@ -240,7 +268,7 @@ export default {
         }
       }      
     },
-  async getFlow(id) {
+  async getFlowsss(id) {
     try {
       this.loading = true
       let res = await axios.get(`loan/${id}/flow`)
@@ -252,57 +280,19 @@ export default {
         this.loading = false
       }
     },
-  async getObservationType() {
-    try {
-      this.loading = true
-      let res = await axios.get(`module/${6}/observation_type`)
-      this.observation_type = res.data
-      console.log('estas son las observaciones'+this.observation_type)
-    } catch (e) {
-      console.log(e)
-    } finally {
-      this.loading = false
-    }
-    },
-  async getRecords() {
-    try {
-      this.loading = true
-      let res = await axios.get(`record`,{param:{
-      loan_id:1
-    }})
-      this.record = res.data.data
-      console.log('este el record'+this.record)
-    } catch (e) {
-      console.log(e)
-    } finally {
+    async getFlow() {
+      try {
+        this.loading = true
+        let response = await axios.get(`loan/${4}/flow`)
+        if (response.data) {
+          this.flow = response.data
+        }
+      } catch (e) {
+        console.log(e)
+      } finally {
         this.loading = false
-    }
-  },
-  async getObservation(id) {
-    try {
-      this.loading = true
-      let res = await axios.get(`loan/${id}/observation`)
-      this.observation = res.data
-      console.log('este la observacion'+this.observation)
-    } catch (e) {
-      console.log(e)
-    } finally {
-      this.loading = false
-    }
-  },
-  async saveObservation() {
-    try {
-      let res = await axios.post(`loan/${1}/observation`, {
-      observation_type_id:2,
-      message:"observacion adicionada"
-    });
-      console.log('se grabo el mensaje')
-    } catch (e) {
-      console.log(e)
-    } finally {
-      this.loading = false
-    }
-  },
+      }
+    },
   async saveLoanRol() {
     try {
       let res = await axios.patch(`loan/${1}`, {
@@ -315,6 +305,8 @@ export default {
       this.loading = false
     }
   },
-  }
+
+  },
+  
   }
 </script>
