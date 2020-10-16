82514c2d9b8184bee47f5c11b042ff6a359dad48
gladys850 <gpq850@gmail.com>
2020-10-16 11:57:45 -0400

    Obtener datos del codeudor (afiliado) y validación  en la modalidad hipotecario

--- resources/assets/js/components/loan/BallotsHipotecary.vue
+++ resources/assets/js/components/loan/BallotsHipotecary.vue
@@ -1,10 +1,8 @@
 <template>
   <v-flex xs12 class="px-3">
-    <div class="text-center">
-      BOLETAS DE PAGO DEL CODEUDOR</div>
+    <div class="text-center">BOLETAS DE PAGO DEL CODEUDOR</div>
     <v-form>
       <v-row justify="center">
-       
         <v-col cols="12">
           <v-container class="py-0">
             <v-row>
@@ -36,28 +34,16 @@
                 </v-tooltip>
               </v-col>
               <v-col>
-              <v-data-table
-                dense
-                :headers="headers1"
-                :items="contrib_codebtor"
-                sort-by
-                class="elevation-1 ma-0 pa-3 pb-6"
-                hide-default-footer
-              >
-                <template v-slot:top>
-                  
+                <v-data-table
+                  dense
+                  :headers="headers1"
+                  :items="contrib_codebtor"
+                  sort-by
+                  class="elevation-1 ma-0 pa-3 pb-6"
+                  hide-default-footer
+                >
+                  <template v-slot:top>
                     <v-dialog v-model="dialog" max-width="500px">
-                      <!--<template v-slot:activator="{ on, attrs }">
-                              <v-btn
-                                fab
-                                dark
-                                x-small
-                                v-on="on"
-                                color="info"
-                                v-bind="attrs"
-                              ><v-icon>mdi-plus</v-icon>
-                              </v-btn>
-                      </template>-->
                       <v-card>
                         <v-card-title class="pa-0">
                           <v-toolbar dense flat color="tertiary">
@@ -71,12 +57,9 @@
                         <v-card-text class="py-0">
                           <v-container>
                             <v-row>
-                              <v-col cols="12" sm="6" md="4">
-                                <v-text-field
-                                  v-model="editedItem.id_affiliate"
-                                  dense
-                                  label="Nombre"
-                                ></v-text-field>
+                              <v-col cols="12" sm="12" md="12" style="color: teal">
+                                <h3>CI: {{editedItem.identity_card}}</h3>
+                                <h3>NOMBRE: {{editedItem.full_name}} </h3>
                               </v-col>
                               <v-col cols="12" sm="6" md="4">
                                 <v-text-field
@@ -122,19 +105,18 @@
                           <v-btn color="blue darken-1" dense text @click="save">Guardar</v-btn>
                         </v-card-actions>
                       </v-card>
-                    </v-dialog>                 
-                </template>
-
+                    </v-dialog>
+                  </template>
+                  <template v-slot:item.request_date="{ item }">{{ item.request_date | date }}</template>
                   <template v-slot:item.actions="{ item }">
-                  <v-icon small class="mr-2" color="blue" @click="editItem(item)">mdi-pencil</v-icon>
-                  <v-icon small color="error" @click="deleteItem(item)">mdi-delete</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <!--<v-btn color="primary" @click=" initialize">Reset</v-btn>-->
-                </template>
-              </v-data-table>
+                    <v-icon small class="mr-2" color="blue" @click="editItem(item)">mdi-pencil</v-icon>
+                    <v-icon small color="error" @click="deleteItem(item)">mdi-delete</v-icon>
+                  </template>
+                  <template v-slot:no-data>
+                    <!--<v-btn color="primary" @click=" initialize">Reset</v-btn>-->
+                  </template>
+                </v-data-table>
               </v-col>
-
             </v-row>
           </v-container>
         </v-col>
@@ -145,7 +127,7 @@
 <script>
 export default {
   name: "ballots_hipotecary",
-  props:{
+  props: {
     modalidad: {
       type: Object,
       required: true
@@ -154,73 +136,83 @@ export default {
       type: Array,
       required: true
     },
-    contrib_codebtor:{
+    contrib_codebtor: {
       type: Array,
+      required: true
+    },
+    affiliate: {
+      type: Object,
       required: true
     }
   },
   data: () => ({
     affiliate_codebtor_ci: null,
-    editar:true,
-    //monto:null,
-    //plazo:null,contrib_codebtor
-    //interval:[],
-    //contributions1:[],
-    //loanTypeSelected:null,
-    //visible:false,
-    //num_type:9,
-    //contributions1:{},
-    //modalidad: {},
+    editar: true,
     bonos: {},
     payable_liquid: {},
-    //modalities: {},
-    //prueba: {},
-    //intervalos: {},
     exist_codebtor: true,
     ver: false,
     dialog: false,
-    //ballots: [],
-    //contrib_codebtor: [],
-    //contrib_codebtor_aux: [],
-
     headers1: [
       {
-        text: "Nombre Afiliado",
-       
-        class: ['normal', 'white--text'],
+        text: "id",
+        class: ["normal", "white--text"],
         sortable: false,
         value: "id_affiliate"
       },
-      { text: "Liquido Pagable", 
-        class: ['normal', 'white--text'],
+      {
+        text: "CI",
+        class: ["normal", "white--text"],
+        sortable: false,
+        value: "identity_card"
+      },
+      {
+        text: "Nombre Afiliado",
+        class: ["normal", "white--text"],
+        sortable: false,
+        value: "full_name"
+      },
+      {
+        text: "Liquido Pagable",
+        class: ["normal", "white--text"],
         value: "payable_liquid"
       },
-      { text: "Bono Frontera", 
-        class: ['normal','white--text'],
-        value: "border_bonus" 
+      {
+        text: "Bono Frontera",
+        class: ["normal", "white--text"],
+        value: "border_bonus"
       },
-      { text: "Bono Oriente", 
-        class: ['normal', 'white--text'],
-        value: "east_bonus" 
+      {
+        text: "Bono Oriente",
+        class: ["normal", "white--text"],
+        value: "east_bonus"
       },
-      { text: "Bono Cargo", 
-        class: ['normal', 'white--text'],
-        value: "seniority_bonus" 
+      {
+        text: "Bono Cargo",
+        class: ["normal", "white--text"],
+        value: "seniority_bonus"
       },
-      { text: "Bono Seguridad Ciudadana", 
-        class: ['normal', 'white--text'],
-        value: "public_security_bonus" 
+      {
+        text: "Bono Seguridad Ciudadana",
+        class: ["normal", "white--text"],
+        value: "public_security_bonus"
       },
-      { text: "Actions", 
-        class: ['normal', 'white--text'],
-        value: "actions", 
+      {
+        text: "Actions",
+        class: ["normal", "white--text"],
+        value: "actions",
         sortable: false
       }
     ],
-    //desserts: [],
     editedIndex: -1,
     editedItem: {
-      id_affiliate: "",
+      id_affiliate: null,
+      identity_card: null,
+      first_name:null,
+      second_name:null,
+      last_name: null,
+      mothers_last_name: null,
+      full_name:null,
       payable_liquid: 0,
       border_bonus: 0,
       east_bonus: 0,
@@ -229,6 +221,12 @@ export default {
     },
     defaultItem: {
       id_affiliate: "por defecto",
+      identity_card: null,
+      first_name:null,
+      second_name:null,
+      last_name: null,
+      mothers_last_name: null,
+      full_name:null,
       payable_liquid: 0,
       border_bonus: 0,
       east_bonus: 0,
@@ -239,30 +237,23 @@ export default {
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "Nueva Boleta" : "Editar Boleta";
+      return this.editedIndex === -1 ? "Nueva Boleta" : "Editar Boleta"
     }
   },
 
   watch: {
     dialog(val) {
-      val || this.close();
+      val || this.close()
     }
   },
-  /*created () {
-      this.getBallots(id)
-    },*/
   beforeMount() {
-    this.getBallots(this.$route.query.affiliate_id);
+    this.getBallots(this.$route.query.affiliate_id)
   },
-  /*mounted() {
-    this.getLoanModality(this.$route.query.affiliate_id);
-  },*/
   methods: {
     //Metodo para sacar boleta de un afiliado
     async getBallots(id) {
       try {
-        let data_ballots=[]
-        let i, j
+        let data_ballots = []
         let res = await axios.get(`affiliate/${id}/contribution`, {
           params: {
             city_id: this.$store.getters.cityId,
@@ -271,137 +262,111 @@ export default {
             per_page: this.modalidad.quantity_ballots, //FIXME ver si en el caso hipotecario la boleta deberia enviarse solo 1
             page: 1
           }
-        });
+        })
         console.log(res)
-         data_ballots = res.data.data;
+        data_ballots = res.data.data
         if (res.data.valid) {
-          this.editar=false
+          this.editar = false
           console.log("RESULTADOS DE BALLOT")
           console.log(this.modalidad.quantity_ballots)
-          console.log(res.data);         
-          console.log(data_ballots);
-          //for (i = 0; i < data_ballots.length; i++) {
-              this.editedItem.id_affiliate = data_ballots[0].affiliate_id,
-              this.editedItem.payable_liquid = data_ballots[0].payable_liquid,
-              this.editedItem.border_bonus = data_ballots[0].border_bonus,
-              this.editedItem.east_bonus = data_ballots[0].east_bonus,
-              this.editedItem.seniority_bonus = data_ballots[0].seniority_bonus,
-              this.editedItem.public_security_bonus = data_ballots[0].public_security_bonus
-          //}
-          /*for (j = 0; j < data_ballots.length; j++) {
-            this.contrib_codebtor[j].id_affiliate = data_ballots[j].affiliate_id;
-            this.contrib_codebtor[j].payable_liquid = data_ballots[j].payable_liquid;
-
-            if (j == 0) {
-              (this.contrib_codebtor[j].border_bonus = data_ballots[j].border_bonus),
-              (this.contrib_codebtor[j].east_bonus = data_ballots[j].east_bonus),
-              (this.contrib_codebtor[j].seniority_bonus = data_ballots[j].seniority_bonus),
-              (this.contrib_codebtor[j].public_security_bonus = data_ballots[j].public_security_bonus);
-            } else {
-              (this.contrib_codebtor[j].border_bonus = 0),
-              (this.contrib_codebtor[j].east_bonus = 0),
-              (this.contrib_codebtor[j].seniority_bonus = 0),
-              (this.contrib_codebtor[j].public_security_bonus = 0);
-            }
-          }*/
-        } else{
-              this.editedItem.id_affiliate = data_ballots[0].affiliate_id,
-              this.editedItem.payable_liquid = 0,
-              this.editedItem.border_bonus = 0,
-              this.editedItem.east_bonus = 0,
-              this.editedItem.seniority_bonus = 0,
-              this.editedItem.public_security_bonus = 0
+          console.log(res.data)
+          console.log(data_ballots)
+          this.editedItem.id_affiliate = data_ballots[0].affiliate_id
+          this.editedItem.payable_liquid = data_ballots[0].payable_liquid
+          this.editedItem.border_bonus = data_ballots[0].border_bonus
+          this.editedItem.east_bonus = data_ballots[0].east_bonus
+          this.editedItem.seniority_bonus = data_ballots[0].seniority_bonus
+          this.editedItem.public_security_bonus = data_ballots[0].public_security_bonus
+        } else {
+          this.editedItem.id_affiliate = data_ballots[0].affiliate_id
+          this.editedItem.payable_liquid = 0
+          this.editedItem.border_bonus = 0
+          this.editedItem.east_bonus = 0
+          this.editedItem.seniority_bonus = 0
+          this.editedItem.public_security_bonus = 0
         }
       } catch (e) {
-        console.log(e);
+        console.log(e)
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     },
-    /*initialize () {
-    this.desserts = []
-  },*/
 
     editItem(item) {
-      this.editedIndex = this.contrib_codebtor.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+      this.editedIndex = this.contrib_codebtor.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
     },
 
     deleteItem(item) {
-      const index = this.contrib_codebtor.indexOf(item);
-      confirm("Esta seguro que?") && this.contrib_codebtor.splice(index, 1);
+      const index = this.contrib_codebtor.indexOf(item)
+      confirm("Esta seguro que?") && this.contrib_codebtor.splice(index, 1)
     },
 
     close() {
-      this.dialog = false;
+      this.dialog = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.contrib_codebtor[this.editedIndex], this.editedItem);
+        Object.assign(this.contrib_codebtor[this.editedIndex], this.editedItem)
       } else {
-        this.contrib_codebtor.push(this.editedItem);
+        this.contrib_codebtor.push(this.editedItem)
       }
       //this.formatear()
-      this.close();
+      this.close()
     },
 
     async searchCodebtor() {
       try {
-        /*     if(this.affiliate_codebtor_ci==this.affiliate.identity_card) //TODO verificar y/o adicionar validaciones
-        {
-          this.toastr.error("El garante no puede tener el mismo carnet que el titular.")
+        if (this.affiliate_codebtor_ci == this.affiliate.identity_card) {
+          //TODO verificar y/o adicionar validaciones
+          this.toastr.error(
+            "El garante no puede tener el mismo carnet que el titular."
+          )
+        } else {
+          let resp = await axios.get(`affiliate_existence`, {
+            params: {
+              identity_card: this.affiliate_codebtor_ci
+            }
+          })
+          this.affiliate_codebtor = resp.data
+          this.exist_codebtor = this.affiliate_codebtor.state
+          this.ver = true
+          console.log("Entro al metodo de codeudor" + this.affiliate_codebtor + "==>" +this.affiliate_codebtor_ci
+        )
         }
-        else{*/
-        let resp = await axios.get(`affiliate_existence`, {
-          params: {
-            identity_card: this.affiliate_codebtor_ci
-          }
-        });
-        this.affiliate_codebtor = resp.data;
-        this.exist_codebtor = this.affiliate_codebtor.state;
-        this.ver = true;
-        console.log("Entro al metodo de codeudor" + this.affiliate_codebtor + "==>" +this.affiliate_codebtor_ci
-        );
-        /*}*/
         if (this.exist_codebtor) {
-          this.getBallots(this.affiliate_codebtor.affiliate.id);
-          console.log("ID DEL AFILIADO " + this.affiliate_codebtor.affiliate.id
-          );
-          this.dialog = true;
+          this.getBallots(this.affiliate_codebtor.affiliate.id)
+          console.log("ID DEL AFILIADO " + this.affiliate_codebtor.affiliate.id)
+          this.getAffiliate(this.affiliate_codebtor.affiliate.id)
+          this.dialog = true
         }
-        
       } catch (e) {
-        console.log(e);
+        console.log(e)
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     },
-    /*formatear() {
-      let nuevoArray = [];
-      let i;
-      for (i = 0; i < this.contrib_codebtor.length; i++) {
-        nuevoArray[i] = {
-          affiliate_id: this.contrib_codebtor[i].id_affiliate,
-          contributions: {
-            payable_liquid: this.contrib_codebtor[i].payable_liquid,
-            border_bonus: this.contrib_codebtor[i].border_bonus,
-            east_bonus: this.contrib_codebtor[i].east_bonus,
-            seniority_bonus: this.contrib_codebtor[i].seniority_bonus,
-            public_security_bonus: this.contrib_codebtor[i].public_security_bonus
-          }
-        };
-        console.log("FORMATEAR");
-        console.log(nuevoArray);
+    async getAffiliate(id){
+      try {
+        let res= await axios.get(`affiliate/${id}`)
+        let data_codebtor = res.data
+        this.editedItem.identity_card = data_codebtor.identity_card
+        this.editedItem.first_name = data_codebtor.first_name
+        this.editedItem.second_name = data_codebtor.second_name
+        this.editedItem.last_name = data_codebtor.last_name
+        this.editedItem.mothers_last_name = data_codebtor.mothers_last_name
+        this.editedItem.full_name = data_codebtor.first_name + " " + data_codebtor.last_name + " " +data_codebtor.mothers_last_name
+        console.log(data_codebtor)
+      } catch (e) {
+        console.log(e)
       }
-      //this.contrib_codebtor_aux = { liquid_calification: nuevoArray };
-      this.contrib_codebtor_aux = nuevoArray;
-    }*/
+    }
   }
-};
+}
 </script>
