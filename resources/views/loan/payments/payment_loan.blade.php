d9951b25bbc6cd1b0e02ffdeca6b52df4c1ead68
Maritza <mary>
2021-02-23 17:33:41 -0400

    modificación de pdf para impresión del registro de cobro

--- resources/views/loan/payments/payment_loan.blade.php
+++ resources/views/loan/payments/payment_loan.blade.php
@@ -19,86 +19,106 @@
     </div>
     <div class="block">
         <table class="table-info w-100 text-center uppercase my-20">
-            <tr class="bg-grey-darker text-xxs text-white">
+            <tr  class="bg-grey-darker text-xxs text-white">
                 <td class="w-25">Código Tŕamite</td>
                 @if ($loan->parent_loan)
                 <td class="w-25">Trámite origen</td>
                 @endif
-                <td class="{{ $loan->parent_loan ? 'w-50' : 'w-75' }}" colspan="{{ $loan->parent_loan ? 1 : 2 }}">Modalidad de trámite</td>
+                <td class="{{ $loan->parent_loan ? 'w-50' : 'w-75' }}" colspan="{{ $loan->parent_loan ? 2 : 3 }}">Modalidad de trámite</td>
             </tr>
             <tr>
                 <td class="data-row py-5">{{ $loan->code }}</td>
                 @if ($loan->parent_loan)
                 <td class="data-row py-5">{{ $loan->parent_loan->code }}</td>
                 @endif
-                <td class="data-row py-5" colspan="{{ $loan->parent_loan ? 1 : 2 }}">{{ $loan->modality->name }}</td>
+                <td class="data-row py-5" colspan="{{ $loan->parent_loan ? 2 : 3 }}">{{ $loan->modality->name }}</td>
             </tr>
             @foreach ($lenders as $lender)
             <tr class="bg-grey-darker text-xxs text-white">
                 <td>Fecha de Desembolso</td>
                 <td>DATOS DE{{ $plural ? ' LOS' : 'L' }} TITULAR{{ $plural ? 'ES' : ''}}</td>
                 <td>CI</td>
+                <td>Matricula</td>
             </tr>
             <tr>
                 <td class="data-row py-5">{{ Carbon::parse($loan->disbursement_date)->format('d/m/y')}}</td>
                 <td class="data-row py-5">{{ $lender->title }} {{ $lender->full_name }}  {{ $is_dead?  '' : $lender->affiliate_state->affiliate_state_type->name}} </td>
                 <td class="data-row py-5">{{ $lender->identity_card_ext }}</td>
+                <td class="data-row py-5">{{ $lender->registration }}</td>      
             </tr>
             @endforeach
         </table>
     </div>
-
-    <div class="block">
+        <div class="block">
         <div class="font-semibold leading-tight text-left m-b-10 text-xs">{{ $n++ }}. DATOS DEL PAGO</div>
     </div>
-
     <div class="block">
-        <table class="table-info w-100 text-center my-20 uppercase">
+    <table class="table-info w-100 text-center my-20 uppercase">
             <tr class="bg-grey-darker text-xxs text-white">
-                <td class="w-30">Número de cuota</td>
-                <td class="w-35">Saldo Anterior</td>
-                <td class="w-35">Fecha de Cálculo</td>
+                <td class="w-10">Número de cuota</td>
+                <td class="w-15">Saldo Anterior</td>
+                <td class="w-15">Saldo Actual</td>
+                <td class="w-15">Fecha de Cálculo</td>
+                <td class="w-15">Fecha de pago</td>
+                <td >Intereses Penales Pendientes</td>
+                <td >Intereses Penales Pendientes</td>
             </tr>
             <tr>
                 <td class="data-row py-5">{{ $loan_payment->quota_number }}</td>
                 <td class="data-row py-5">{{ $loan->balance }}</td>
+                <td class="data-row w-10">completar</td>
                 <td class="data-row py-5">{{ Carbon::parse($loan_payment->estimated_date)->format('d/m/y') }}</td>
+                <td class="data-row py-5">28/02/21</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+    </div>
+    <div class="block">
+        <table class="table-info w-100 my-20 uppercase">
+            <tr class="bg-grey-darker text-center text-xxs text-white">   
+               <td colspan="5">Pago</td>
+           </tr>
+            <tr>
+                <td colspan="2" class="w-25 text-left">Amortizacion a Capital</td>
+                <td class="w-10 text-right">{{ Util::money_format($loan_payment->capital_payment) }}</td>
             </tr>
             <tr class="">
-                <td colspan="2" class="text-left p-10 w-50">Otros Cobros (Interes Acumulado previo, interes penal previo, interes acumulado)</td>
+                <td class="w-25">Intereses por</td>
+                <td class="w-10">44 dias a 13.2% anual</td>
+                <td class="w-10 text-right">{{ Util::money_format($loan_payment->interest_payment) }}</td>             
+            </tr>
+            <tr class="">
+                <td class="w-30">Intereses Penales por</td>
+                <td class="w-10">13 dias a 6% anual</td>
+                <td class="w-10 text-right">{{ Util::money_format($loan_payment->penal_payment) }}</td>            </tr>
+            <tr class="">
+                <td class="w-30">Intereses Corrientes Pendientes</td>
+                <td colspan="2" class="w-50 text-right">37.20</td>
+            </tr>
+            <tr class="">
+                <td class="w-30">Intereses Penales Pendientes</td>
+                <td colspan="2" class="w-30 text-right">0</td>                        
+                </tr>
+            <tr class="">
+                <td class="text-left">Otros Cobros (Interes Acumulado previo, interes penal previo, interes acumulado)</td>
                 @php ($total_accumulated = 0)
                 @php ($total_accumulated = $loan_payment->accumulated_remaining+$loan_payment->accumulated_payment+$loan_payment->penal_remaining)
-                <td class="w-50">{{ Util::money_format($total_accumulated) }}</td>
+                <td colspan="2" class="text-right">{{ Util::money_format($total_accumulated) }}</td>            
             </tr>
-            <tr class="">
-                <td colspan="2" class="text-left p-10 w-50">Interes Penal</td>
-                <td class="w-50">{{ Util::money_format($loan_payment->penal_payment) }}</td>
-
-            </tr>
-            <tr class="">
-                <td colspan="2" class="text-left p-10 w-50">Interes Corriente</td>
-                <td class="w-50">{{ Util::money_format($loan_payment->interest_payment) }}</td>
-
-            </tr>
-            <tr class="">
-                <td colspan="2" class="text-left p-10 w-50">Amortizacion a Capital</td>
-                <td class="w-50">{{ Util::money_format($loan_payment->capital_payment) }}</td>
-
-            </tr>
+        
             <tr class="font-semibold leading-tight">
-                <td colspan="2" class="text-left p-10 w-50">Total a Pagar (<span class="uppercase font-semibold leading-tight  m-b-10 text-xs">{{ Util::money_format($loan_payment->estimated_quota, true) }} Bolivianos</span> )
+                <td class="text-left p-10"><div>Total a Pagar:</div><div>Son:(<span class="uppercase font-semibold leading-tight  m-b-10 text-xs">{{ Util::money_format($loan_payment->estimated_quota, true) }} Bolivianos</span> )</div>
                 </td>
-                <td class="w-50">{{ Util::money_format($loan_payment->estimated_quota) }}</td>
+                <td colspan="2" class="text-right">{{ Util::money_format($loan_payment->estimated_quota) }}</td>
             </tr>
             @if($loan_payment->description)
             <tr>
-                <td colspan="3" class="text-left p-10"><span class="font-semibold">Observaciones</span> <br>
+                <td  class="text-left p-10"><span class="font-semibold">Observaciones</span> <br>
                     {{$loan_payment->description}}
                 </td>
 
             </tr>
             @endif
-
         </table>
     </div>
     <div class="m-t-25 no-page-break">
