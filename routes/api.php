78910ae3068af869304ccc6ffb3695d4f20809ba
Noemi Ancari <noemiancari.na@gmail.com>
2021-04-21 17:53:29 -0400

    Importacion de pendientes por confirmar

--- routes/api.php
+++ routes/api.php
@@ -196,6 +196,7 @@ Route::group([
             Route::patch('loan/{loan}/payment','Api\V1\LoanController@get_next_payment');
             Route::post('loan/{loan}/payment','Api\V1\LoanController@set_payment');
             Route::post('loan_payment/importation_command_senasir', 'Api\V1\LoanPaymentController@importation_command_senasir');//importacion de pagos
+            Route::post('loan_payment/importation_pending_command_senasir', 'Api\V1\LoanPaymentController@importation_pending_command_senasir');//importacion de pendientes de pagos
         });
         Route::group([
             'middleware' => 'permission:update-payment-loan'
