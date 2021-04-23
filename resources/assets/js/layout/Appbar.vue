76a2bbc36df234aa4314642fabbc6869c30b989f
gladys850 <gpq850@gmail.com>
2021-04-23 11:20:37 -0400

    Crear métodos globales en el store para obtener los roles y permisos en el proyecto, Incorporar selector de rol para el cambio

--- resources/assets/js/layout/Appbar.vue
+++ resources/assets/js/layout/Appbar.vue
@@ -11,6 +11,20 @@
     <v-app-bar-nav-icon @click.stop="$emit('update:expanded', !expanded)"></v-app-bar-nav-icon>
     <v-toolbar-title>{{ bar.text }}</v-toolbar-title>
     <v-spacer></v-spacer>
+    <div width="300px">
+      <v-select
+        outlined
+        hide-details
+        v-model="rolePermissionSelected"
+        :items="rolesPermissionsItems"
+        label="Rol actual"
+        class="py-0 my-0"
+        item-text="display_name"
+        return-object
+        dense
+      ></v-select>
+    </div>
+    <!-- <span>{{ roles }}aaa</span> -->
     <LoggedUser/>
   </v-app-bar>
 </template>
@@ -29,6 +43,44 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      rolesPermissionsItems: [],
+      rolePermissionSelected: null,
+    }
+  },
+  async created() {
+    await this.getRolePermissions()
+    this.setDefaultValues()
+  },
+  methods: {
+    async getRolePermissions() {
+      try {
+        let res = await axios.get(`user_role/${6}/permission`)
+        let aux_rolesPermissionsItems = res.data
+        aux_rolesPermissionsItems.forEach(item => {
+          //delete item.id
+          delete item.module_id
+          delete item.action
+          delete item.created_at
+          delete item.updated_at
+          delete item.correlative
+          delete item.name
+          delete item.sequence_number
+          item.permissions = item.permissions.map(item => ({display_name: item.display_name, name: item.name }))
+        })
+        this.rolesPermissionsItems =  aux_rolesPermissionsItems
+        console.log(this.rolesPermissionsItems)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    setDefaultValues() {
+      if(this.rolesPermissionsItems.length > 0) {
+        this.rolePermissionSelected = this.rolesPermissionsItems[0]
+      }
+    },
+  },
   computed: {
     bar() {
       if (process.env.NODE_ENV != 'production') {
@@ -42,6 +94,14 @@ export default {
           text: `PLATAFORMA VIRTUAL DE TRÁMITES`
         }
       }
+    },
+    // roles() {
+    //   return this.$store.getters.roles
+    // },
+  },
+  watch: {
+    'rolePermissionSelected.display_name'(val) {
+      this.$store.commit('setRolePermissionSelected', this.rolePermissionSelected)
     }
   }
 }
