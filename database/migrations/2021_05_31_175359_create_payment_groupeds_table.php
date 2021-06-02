44863a1452ab65dc0b67bc58f0312e634ccf6a78
Maritza <marycqmia@gmail.com>
2021-06-02 09:01:23 -0400

    Creación de las tablas import payments, payment_groupeds, import_payment_groupeds para la importación

---
+++ database/migrations/2021_05_31_175359_create_payment_groupeds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentGroupedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_groupeds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('affiliate_id')->unsigned();
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+            $table->unsignedBigInteger('period_id')->unsigned();
+            $table->foreign('period_id')->references('id')->on('periods');
+            $table->float('amount',10,2);
+            $table->float('amount_balance',10,2);
+            $table->enum('origin', ['C', 'S']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_groupeds');
+    }
+}
