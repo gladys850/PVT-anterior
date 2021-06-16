956f5378d5d5467ee4687a11ec24899bf13fba33
Stephanie Luna <stephanielunaquevedo@gmail.com>
2021-06-16 17:27:25 -0400

    Se añadio el boton de imprimir recibo para anticipo

--- resources/assets/js/components/tracing/Dashboard.vue
+++ resources/assets/js/components/tracing/Dashboard.vue
@@ -3,44 +3,41 @@
     <v-row>
       <v-col cols="12" class="text-center">
         <v-row>
-             <v-col cols="12" class="text-center py-0" style="margin-bottom:10px">
+          <v-col cols="12" class="text-center py-0" style="margin-bottom:10px">
             <v-card class="py-0" color="#151515" dark max-width="100%" max-height="500">
               <v-card-text class="headline font-weight-bold">
                 <v-row>
-                <v-col cols="5" class="py-0">
-                   <h6><v-icon large left style="font-size: 25px;">
-                    mdi-shield-account
-                  </v-icon>
-                 <strong><b style="color:white">PRESTATARIO: </b></strong>
-                  {{ $options.filters.fullName(affiliate, true) }}</h6>
-                   <h6><strong><b style="color:white">CI: </b></strong>
-                  {{ affiliate.identity_card }}</h6>
+                  <v-col cols="5" class="py-0">
+                    <h6><v-icon large left style="font-size: 25px;">
+                      mdi-shield-account
+                    </v-icon>
+                    <strong><b style="color:white">PRESTATARIO: </b></strong>
+                      {{ $options.filters.fullName(affiliate, true) }}</h6>
+                    <h6><strong><b style="color:white">CI: </b></strong>
+                      {{ affiliate.identity_card }}</h6>
                     <div v-if="loan.disbursable_type == 'spouses'">
-               <h6> <v-icon large left style="font-size: 25px;">
-                    mdi-account-heart
-                  </v-icon>
-                  <strong><b style="color:white">CONYUGUE:</b></strong> {{ $options.filters.fullName(spouse, true) }}
-                  <b style="color:white">C.I: </b> {{ spouse.identity_card }}</h6>
-            
-          
-            </div>
-               <div v-for="(lenders,i) in loan.lenders" :key="i" v-show="loan.lenders.length > 1">
-                 <h6><strong><b style="color:white">CODEUDOR: </b>{{  $options.filters.fullName(lenders,true)}}</strong>
-                  <b style="color:white">C.I: </b>{{lenders.identity_card}} </h6>
-              
-              </div>
-                </v-col>
-                <v-col cols="7" class="py-0 ">
-                   <h6><v-icon large left style="font-size: 25px;">
-                 mdi-bank
-                </v-icon>
-                <strong><b style="color:white">MODALIDAD:</b></strong>
-                {{ procedure_modality_name | uppercase }}</h6>
-                <h6><strong><b style="color:white">MONTO SOLICITADO:</b></strong>
-                {{ loan.amount_approved | money}}  Bs
-                  <strong><b style="color:white">MESES PLAZO:</b></strong>
-                {{ loan.loan_term }}</h6>
-                </v-col>
+                      <h6><v-icon large left style="font-size: 25px;">
+                        mdi-account-heart
+                        </v-icon>
+                      <strong><b style="color:white">CONYUGUE:</b></strong> {{ $options.filters.fullName(spouse, true) }}
+                      <b style="color:white">C.I: </b> {{ spouse.identity_card }}</h6>
+                    </div>
+                    <div v-for="(lenders,i) in loan.lenders" :key="i" v-show="loan.lenders.length > 1">
+                      <h6><strong><b style="color:white">CODEUDOR: </b>{{  $options.filters.fullName(lenders,true)}}</strong>
+                      <b style="color:white">C.I: </b>{{lenders.identity_card}} </h6>
+                    </div>
+                  </v-col>
+                  <v-col cols="7" class="py-0 ">
+                    <h6><v-icon large left style="font-size: 25px;">
+                    mdi-bank
+                    </v-icon>
+                    <strong><b style="color:white">MODALIDAD:</b></strong>
+                      {{ procedure_modality_name | uppercase }}</h6>
+                    <h6><strong><b style="color:white">MONTO SOLICITADO:</b></strong>
+                      {{ loan.amount_approved | money}}  Bs
+                    <strong><b style="color:white">MESES PLAZO:</b></strong>
+                      {{ loan.loan_term }}</h6>
+                  </v-col>
                 </v-row>
               </v-card-text>
             </v-card>
