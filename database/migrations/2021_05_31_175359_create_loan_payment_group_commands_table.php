53abcfa934d3e895c963e31cc34a8db2c286614d
Noemi Ancari <noemiancari.na@gmail.com>
2021-06-30 09:27:49 -0400

    Cambio de nombre de las migraciones y tablas

--- database/migrations/2021_05_31_175359_create_loan_payment_group_commands_table.php
+++ database/migrations/2021_05_31_175359_create_loan_payment_group_commands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommandPaymentGroupsTable extends Migration
+class CreateLoanPaymentGroupCommandsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCommandPaymentGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('command_payment_groups', function (Blueprint $table) {
+        Schema::create('loan_payment_group_commands', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('affiliate_id')->unsigned();
             $table->foreign('affiliate_id')->references('id')->on('affiliates');
@@ -36,6 +36,6 @@ class CreateCommandPaymentGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('command_payment_groups');
+        Schema::dropIfExists('loan_payment_group_commands');
     }
 }
--- database/migrations/2021_06_23_095026_create_loan_payment_group_senasirs_table.php
+++ database/migrations/2021_06_23_095026_create_loan_payment_group_senasirs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSenasirPaymentGroupsTable extends Migration
+class CreateLoanPaymentGroupSenasirsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSenasirPaymentGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('senasir_payment_groups', function (Blueprint $table) {
+        Schema::create('loan_payment_group_senasirs', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('affiliate_id')->unsigned();
             $table->foreign('affiliate_id')->references('id')->on('affiliates');
@@ -39,6 +39,6 @@ class CreateSenasirPaymentGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('senasir_payment_groups');
+        Schema::dropIfExists('loan_payment_group_senasirs');
     }
 }
