4dcad57a9eb0a14bf3325c747cfc4096ab38da6f
Stephanie Luna <stephanielunaquevedo@gmail.com>
2021-07-27 08:50:52 -0400

    closed #2677

--- resources/assets/js/components/dashboard/DataLoanAffiliate.vue
+++ resources/assets/js/components/dashboard/DataLoanAffiliate.vue
@@ -4,71 +4,6 @@
       <v-container class="py-0">
         <v-card color="grey lighten-1" class="ma-0 pa-3">
           <v-row>
-            <!--v-col cols="12" md="4">
-              <v-card>
-                <v-container class="py-0">
-                  <v-row>
-                    <v-col cols="12" md="4"></v-col>
-                    <v-col cols="12" md="6"> Afiliado </v-col>
-                    <v-col cols="12" md="2"></v-col>
-                    <v-col cols="12" md="1"></v-col>
-                    <v-col cols="12" md="8">
-                      <v-text-field
-                        dense
-                        label="CI ó Matrícula"
-                        v-model="affiliate_ci"
-                        class="py-0"
-                        single-line
-                        hide-details
-                        clearable
-                        :loading="loading"
-                        @keyup.enter="getCalculator()"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="2">
-                      <v-tooltip top>
-                        <template v-slot:activator="{ on }">
-                          <v-btn
-                            fab
-                            dark
-                            x-small
-                            v-on="on"
-                            color="info"
-                            @click.stop="getCalculator()"
-                          >
-                            <v-icon>mdi-magnify</v-icon>
-                          </v-btn>
-                        </template>
-                        <span>Buscar afiliado</span>
-                      </v-tooltip>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card>
-            </!--v-col>
-            <v-col-- cols="12" md="8" v-show="ver">
-              <v-card>
-                <v-card-text>
-                  <v-row>
-                    <v-col   class="text-center" cols="12" md="12" >
-                      <h3 class="info--text aling-text--center " >
-                      DATOS DEL AFILIADO  {{loans1}}
-                      </h3>
-                    </v-col>
-                     <v-progress-linear></v-progress-linear>
-                    <v-col cols="12" md="6" >
-                        Nombre: {{loans1.affiliate}}
-                    </v-col>
-                    <v-col cols="12" md="6" >
-                        C.I.: {{loans1.affiliate_identity_card}}
-                    </v-col>
-                    <v-col cols="12" md="6" >
-                     Estado: {{loans1.state_affiliate}}
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-              </v-card>
-            </v-col-->
             <v-col  cols="12" md="12" v-show="evaluacion">
               <v-row>
                 <v-col cols="12" md="4">
@@ -99,7 +34,7 @@
                         <v-col cols="12" md="4" >
                             {{ anticipo.maximum_term +" meses"}}
                         </v-col>
-                        <v-col cols="12" md="8" >                            
+                        <v-col cols="12" md="8" >
                             Cuota Calculada:
                         </v-col>
                         <v-col cols="12" md="4" >
@@ -143,7 +78,7 @@
                         <v-col cols="12" md="4" >
                             {{ corto_plazo.maximum_term +" meses"}}
                         </v-col>
-                        <v-col cols="12" md="8" >                            
+                        <v-col cols="12" md="8" >
                             Cuota Calculada:
                         </v-col>
                         <v-col cols="12" md="4" >
@@ -187,7 +122,7 @@
                         <v-col cols="12" md="4" >
                             {{ largo_plazo.maximum_term +" meses"}}
                         </v-col>
-                        <v-col cols="12" md="8" >                            
+                        <v-col cols="12" md="8" >
                             Cuota Calculada:
                         </v-col>
                         <v-col cols="12" md="4" >
@@ -255,75 +190,6 @@ export default {
       type: Boolean,
       required: true
     },
-  },
-  data: () => ({
-    //affiliate_ci:null,
-    //loading: false,
-    //largo_plazo:{},
-    //corto_plazo:{},
-    //anticipo:{},
-    
-    //loans1: {
-    //  message:{
-    //    accomplished:null
-    //  },
-    //  state_affiliate:{
-    //    name:null,
-  
-    //  }
-   
-    //},
-    //ver: false,
-    //evaluacion:false
-  }),
-
-  methods: {
-   /* async getCalculator() {
-      try {
-        this.loading = false;
-         this.ver= true
-          let res = await axios.post(`search_loan`, {
-            identity_card : this.affiliate_ci });
-            this.loans1=res.data
-            this.cont=this.loans1.modalities.length
-            for (let i = 0; i < this.loans1.modalities.length; i++)
-             {
-               if(this.loans1.modalities[i].modality_affiliate.procedure_type_id==9)
-               {
-                  this.anticipo.name=this.loans1.modalities[i].name_procedure_modality
-                  this.anticipo.amount_max=this.loans1.modalities[i].amount_max
-                  this.anticipo.maximum_term=this.loans1.modalities[i].modality_affiliate.procedure_type.interval.maximum_term
-                  this.anticipo.liquid_calification=this.loans1.modalities[i].liquid_calification
-                  this.anticipo.annual_interest=this.loans1.modalities[i].interest.annual_interest
-               }
-               if(this.loans1.modalities[i].modality_affiliate.procedure_type_id==10)
-               {
-                  this.corto_plazo.name=this.loans1.modalities[i].name_procedure_modality
-                  this.corto_plazo.amount_max=this.loans1.modalities[i].amount_max
-                  this.corto_plazo.maximum_term=this.loans1.modalities[i].modality_affiliate.procedure_type.interval.maximum_term
-                  this.corto_plazo.liquid_calification=this.loans1.modalities[i].liquid_calification
-                  this.corto_plazo.annual_interest=this.loans1.modalities[i].interest.annual_interest
-               }
-               if(this.loans1.modalities[i].modality_affiliate.procedure_type_id==11)
-               {
-                  this.largo_plazo.name=this.loans1.modalities[i].name_procedure_modality
-                  this.largo_plazo.amount_max=this.loans1.modalities[i].amount_max
-                  this.largo_plazo.maximum_term=this.loans1.modalities[i].modality_affiliate.procedure_type.interval.maximum_term
-                  this.largo_plazo.liquid_calification=this.loans1.modalities[i].liquid_calification
-                  this.largo_plazo.annual_interest=this.loans1.modalities[i].interest.annual_interest
-               }
-
-             }
-            
-            this.evaluacion=res.data.evaluate
-              this.$forceUpdate()
-      } catch (e) {
-        console.log(e);
-      } finally {
-        this.loading = false;
-      }
-    },*/
-
   },
 };
 </script>
