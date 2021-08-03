93c17d9f04a8b5037c1fafdf794ac3ea795c4b9a
Maritza <marycqmia@gmail.com>
2021-08-03 14:14:22 -0400

    ajuste del reporte del listado de prestamos de comando

--- app/Http/Controllers/Api/V1/ImportationReportController.php
+++ app/Http/Controllers/Api/V1/ImportationReportController.php
@@ -280,9 +280,8 @@ class ImportationReportController extends Controller
          $state_service_id = AffiliateState::whereName('Servicio')->first()->id;
          $state_disp_id = AffiliateState::whereName('Disponibilidad')->first()->id;
          //todos los prestamos menores o iguales a la fecha de corte
-         $current_loans =  "select lo.id from affiliates as af,loans as lo, affiliate_states as afs, loan_affiliates as laf where af.id = lo.affiliate_id and lo.affiliate_id = laf.affiliate_id and laf.type='affiliates'
-         and lo.state_id = $loan_state and affiliate_state_id in($state_service_id,$state_disp_id) and CAST(lo.disbursement_date AS date) <= CAST('$estimated_date' AS date) 
-         and afs.id = af.affiliate_state_id"; 
+         $current_loans =  "select id_loan as id from view_loan_borrower
+         where  CAST(disbursement_date_loan AS date) <= CAST('$estimated_date' AS date) and state_affiliate in('Servicio','Disponibilidad') and state_loan ='Vigente'"; 
          $current_loans = DB::select($current_loans);
          $data = array(
             array("Nro Préstamo", "Fecha de desembolso", "Ciudad", "tipo", "Matricula Titular",
